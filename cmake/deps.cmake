a65754c91dd18dd88522bcfe73571a9228179e8b
ouguoyu <ouguoyu@sensetime.com>
2021-07-07 20:51:43 +0800

    update ppl.common dep to v0.1.2

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -50,7 +50,7 @@ set(PPLCOMMON_BUILD_BENCHMARK OFF CACHE BOOL "disable ppl.common benchmark")
 
 hpcc_declare_git_dep(ppl.common
     https://github.com/openppl-public/ppl.common.git
-    v0.1.0)
+    v0.1.2)
 
 # --------------------------------------------------------------------------- #
 
