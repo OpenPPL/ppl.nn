e9c376982a07dd340162b2f55976f66b28d0cb1f
ouguoyu <ouguoyu@sensetime.com>
2021-07-27 23:01:02 +0800

    export x86 config options for python

--- python/engines/x86/py_x86_engine.cc
+++ python/engines/x86/py_x86_engine.cc
@@ -17,6 +17,7 @@
 
 #include "py_x86_engine.h"
 #include "ppl/common/retcode.h"
+#include "ppl/nn/engines/x86/x86_options.h"
 #include "pybind11/pybind11.h"
 #include "pybind11/stl.h"
 #include "ppl/nn/common/logger.h"
@@ -25,7 +26,11 @@ using namespace ppl::common;
 namespace ppl { namespace nn { namespace python {
 
 RetCode PyX86Engine::Configure(uint32_t option, const pybind11::args& args) {
-    LOG(ERROR) << "Configure() is not available now.";
+    if (option == x86::X86_CONF_DISABLE_AVX512) {
+        return engine_->Configure(x86::X86_CONF_DISABLE_AVX512);
+    }
+
+    LOG(ERROR) << "unsupported option: " << option;
     return RC_UNSUPPORTED;
 }
 
@@ -33,6 +38,9 @@ void RegisterX86Engine(pybind11::module* m) {
     pybind11::class_<PyX86Engine>(*m, "X86Engine")
         .def("GetName", &PyX86Engine::GetName)
         .def("Configure", &PyX86Engine::Configure);
+
+    m->attr("X86_CONF_DISABLE_AVX512") = (uint32_t)x86::X86_CONF_DISABLE_AVX512;
+    m->attr("X86_CONF_MAX") = (uint32_t)x86::X86_CONF_MAX;
 }
 
 }}} // namespace ppl::nn::python
