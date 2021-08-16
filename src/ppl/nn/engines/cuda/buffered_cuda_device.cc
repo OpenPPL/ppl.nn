2e7fb6f4c43d6b0710715e85fc5b137d83afde58
xusi <xusi@sensetime.com>
2021-08-16 07:19:29 +0000

    add pretest for engine deconstructor

--- src/ppl/nn/engines/cuda/buffered_cuda_device.cc
+++ src/ppl/nn/engines/cuda/buffered_cuda_device.cc
@@ -64,9 +64,11 @@ RetCode BufferedCudaDevice::Init(const CudaEngineOptions& options, const MemoryM
 }
 
 BufferedCudaDevice::~BufferedCudaDevice() {
-    LOG(DEBUG) << "buffer manager[" << buffer_manager_->GetName() << "] allocates ["
-               << buffer_manager_->GetAllocatedBytes() << "] bytes.";
-    buffer_manager_->Free(&shared_tmp_buffer_);
+    if (buffer_manager_.get()) {
+        LOG(DEBUG) << "buffer manager[" << buffer_manager_->GetName() << "] allocates ["
+                << buffer_manager_->GetAllocatedBytes() << "] bytes.";
+        buffer_manager_->Free(&shared_tmp_buffer_);
+    }
     buffer_manager_.reset();
     allocator_.reset();
 }
