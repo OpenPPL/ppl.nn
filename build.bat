5c9788b390750d76d95ff5828e314269ed8195eb
ouguoyu <ouguoyu@sensetime.com>
2021-09-29 14:55:32 +0800

    [fix] installation on windows

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 md pplnn-build
 cd pplnn-build
-cmake %* -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install ..
+cmake %* -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install ..
 cmake --build . --config Release
-cmake --install . --prefix install
+cmake --install . --prefix %cd%/install
 cd ..
