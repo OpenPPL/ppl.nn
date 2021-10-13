c78c4f643a667439760f7ffa456d9a736679e8ff
ouguoyu <ouguoyu@sensetime.com>
2021-10-13 12:45:42 +0800

    [style][x86] rename cmake target name `PPLKernelX86` to `pplkernelx86_static`

--- cmake/x86.cmake
+++ cmake/x86.cmake
@@ -2,6 +2,6 @@ file(GLOB_RECURSE PPLNN_X86_SRC src/ppl/nn/engines/x86/*.cc)
 list(APPEND PPLNN_SOURCES ${PPLNN_X86_SRC})
 
 add_subdirectory(src/ppl/nn/engines/x86/impls)
-list(APPEND PPLNN_LINK_LIBRARIES PPLKernelX86)
+list(APPEND PPLNN_LINK_LIBRARIES pplkernelx86_static)
 
 list(APPEND PPLNN_COMPILE_DEFINITIONS PPLNN_USE_X86)
