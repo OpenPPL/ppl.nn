26e9d03ed078a26f8aae7a21f7ae899e98662d2e
ouguoyu <ouguoyu@sensetime.com>
2021-10-26 18:07:43 +0800

    [fix] correct opset ranges of onnx ops

--- docs/en/supported-ops-and-platforms.md
+++ docs/en/supported-ops-and-platforms.md
@@ -4,94 +4,96 @@
 2. x86 only supports FP32 precision on AVX512/FMA.
 
 
-## Supported operators
+## Supported operators and opsets
 
 * ONNX
 
-| Op Type            | Op Set | Linux X86-64 | Linux CUDA |
-|:------------------:|:------:|:------------:|:----------:|
-| Add                | 11     | &check;      | &check;    |
-| And                | 11     | &check;      | &check;    |
-| ArgMax             | 11     | &check;      | &check;    |
-| AveragePool        | 11     | &check;      | &check;    |
-| BatchNormalization | 11     | &check;      | &check;    |
-| Cast               | 11     | &check;      | &check;    |
-| Ceil               | 11     | &check;      | &check;    |
-| Clip               | 11     | &check;      | &check;    |
-| Concat             | 11     | &check;      | &check;    |
-| Constant           | 11     | &check;      |            |
-| ConstantOfShape    | 11     | &check;      | &check;    |
-| Conv               | 11     | &check;      | &check;    |
-| ConvTranspose      | 11     | &check;      | &check;    |
-| DepthToSpace       | 11     | &check;      | &check;    |
-| Div                | 11     | &check;      | &check;    |
-| Equal              | 11     | &check;      | &check;    |
-| Exp                | 11     | &check;      | &check;    |
-| Expand             | 11     | &check;      | &check;    |
-| Flatten            | 11     | &check;      | &check;    |
-| Floor              | 11     | &check;      | &check;    |
-| Gather             | 11     | &check;      | &check;    |
-| GatherND           | 11     | &check;      | &check;    |
-| Gemm               | 11     | &check;      | &check;    |
-| Greater            | 11     | &check;      | &check;    |
-| Identity           | 11     | &check;      | &check;    |
-| If                 | 13     | &check;      | &check;    |
-| LeakyRelu          | 11     | &check;      | &check;    |
-| Less               | 11     | &check;      | &check;    |
-| Log                | 11     | &check;      | &check;    |
-| Loop               | 13     | &check;      | &check;    |
-| MatMul             | 11     | &check;      |            |
-| Max                | 11     | &check;      | &check;    |
-| MaxPool            | 11     | &check;      | &check;    |
-| MaxUnpool          | 11     | &check;      | &check;    |
-| Min                | 11     | &check;      | &check;    |
-| Mul                | 11     | &check;      | &check;    |
-| NonMaxSuppression  | 11     | &check;      | &check;    |
-| NonZero            | 11     | &check;      | &check;    |
-| Not                | 11     | &check;      | &check;    |
-| Pad                | 11     | &check;      | &check;    |
-| Pow                | 11     | &check;      | &check;    |
-| ReduceMax          | 11     | &check;      | &check;    |
-| ReduceMean         | 11     | &check;      | &check;    |
-| ReduceMin          | 11     | &check;      | &check;    |
-| ReduceProd         | 11     | &check;      | &check;    |
-| ReduceSum          | 11     | &check;      | &check;    |
-| Relu               | 11     | &check;      | &check;    |
-| Reshape            | 11     | &check;      | &check;    |
-| Resize             | 11     | &check;      | &check;    |
-| RoiAlign           | 11     | &check;      | &check;    |
-| ScatterElements    | 11     | &check;      | &check;    |
-| ScatterND          | 11     | &check;      | &check;    |
-| SequenceAt         | 13     | &check;      | &check;    |
-| Shape              | 11     | &check;      | &check;    |
-| Sigmoid            | 11     | &check;      | &check;    |
-| Size               | 11     | &check;      | &check;    |
-| Slice              | 11     | &check;      | &check;    |
-| Softmax            | 11     | &check;      | &check;    |
-| Split              | 11     | &check;      | &check;    |
-| SplitToSequence    | 13     | &check;      | &check;    |
-| Sqrt               | 11     | &check;      | &check;    |
-| Squeeze            | 11     | &check;      | &check;    |
-| Sub                | 11     | &check;      | &check;    |
-| Sum                | 11     | &check;      | &check;    |
-| Tanh               | 11     | &check;      | &check;    |
-| TopK               | 11     | &check;      | &check;    |
-| Transpose          | 11     | &check;      | &check;    |
-| Unsqueeze          | 11     | &check;      | &check;    |
-| Where              | 11     | &check;      | &check;    |
+| Op Type            | Op Set | Linux/Windows/Darwin X86-64 | Linux/Windows CUDA |
+|:------------------:|:------:|:---------------------------:|:------------------:|
+| Add                | 7~12   | &check;                     | &check;            |
+| And                | 7~16   | &check;                     | &check;            |
+| ArgMax             | 11     | &check;                     | &check;            |
+| AveragePool        | 11~16  | &check;                     | &check;            |
+| BatchNormalization | 9~13   | &check;                     | &check;            |
+| Cast               | 9~12   | &check;                     | &check;            |
+| Ceil               | 6~12   | &check;                     | &check;            |
+| Clip               | 11     | &check;                     | &check;            |
+| Concat             | 11~12  | &check;                     | &check;            |
+| Constant           | 11     | &check;                     |                    |
+| ConstantOfShape    | 9~16   | &check;                     | &check;            |
+| Conv               | 11~16  | &check;                     | &check;            |
+| ConvTranspose      | 11~16  | &check;                     | &check;            |
+| DepthToSpace       | 11~12  | &check;                     | &check;            |
+| Div                | 7~12   | &check;                     | &check;            |
+| Equal              | 11~12  | &check;                     | &check;            |
+| Exp                | 6~12   | &check;                     | &check;            |
+| Expand             | 8~12   | &check;                     | &check;            |
+| Flatten            | 11~12  | &check;                     | &check;            |
+| Floor              | 11~12  | &check;                     | &check;            |
+| Gather             | 11~12  | &check;                     | &check;            |
+| GatherND           | 11     | &check;                     | &check;            |
+| Gemm               | 11~12  | &check;                     | &check;            |
+| Greater            | 9~12   | &check;                     | &check;            |
+| Identity           | 1~12   | &check;                     | &check;            |
+| If                 | 11~12  | &check;                     | &check;            |
+| LeakyRelu          | 6~16   | &check;                     | &check;            |
+| Less               | 9~12   | &check;                     | &check;            |
+| Log                | 6~12   | &check;                     | &check;            |
+| Loop               | 11~12  | &check;                     | &check;            |
+| LSTM               | 7~13   | &check;                     | &check;            |
+| MatMul             | 9~12   | &check;                     | &check;            |
+| Max                | 8~11   | &check;                     | &check;            |
+| MaxPool            | 11     | &check;                     | &check;            |
+| MaxUnpool          | 11~16  | &check;                     | &check;            |
+| Min                | 8~11   | &check;                     | &check;            |
+| Mul                | 7~12   | &check;                     | &check;            |
+| NonMaxSuppression  | 11~16  | &check;                     | &check;            |
+| NonZero            | 9~12   | &check;                     | &check;            |
+| Not                | 1~16   | &check;                     | &check;            |
+| Pad                | 11~12  | &check;                     | &check;            |
+| Pow                | 7~11   | &check;                     | &check;            |
+| Range              | 11~16  | &check;                     | &check;            |
+| ReduceMax          | 11     | &check;                     | &check;            |
+| ReduceMean         | 11~12  | &check;                     | &check;            |
+| ReduceMin          | 11     | &check;                     | &check;            |
+| ReduceProd         | 11~12  | &check;                     | &check;            |
+| ReduceSum          | 11~12  | &check;                     | &check;            |
+| Relu               | 6~12   | &check;                     | &check;            |
+| Reshape            | 5~12   | &check;                     | &check;            |
+| Resize             | 11~12  | &check;                     | &check;            |
+| RoiAlign           | 10~15  | &check;                     | &check;            |
+| ScatterElements    | 11~12  | &check;                     | &check;            |
+| ScatterND          | 11~12  | &check;                     | &check;            |
+| SequenceAt         | 11~16  | &check;                     | &check;            |
+| Shape              | 1~12   | &check;                     | &check;            |
+| Sigmoid            | 6~12   | &check;                     | &check;            |
+| Slice              | 11~12  | &check;                     | &check;            |
+| Softmax            | 11~12  | &check;                     | &check;            |
+| Split              | 11~12  | &check;                     | &check;            |
+| SplitToSequence    | 11~16  | &check;                     | &check;            |
+| Sqrt               | 6~12   | &check;                     | &check;            |
+| Squeeze            | 11~12  | &check;                     | &check;            |
+| Sub                | 7~12   | &check;                     | &check;            |
+| Sum                | 8~12   | &check;                     |                    |
+| Tanh               | 6~12   | &check;                     | &check;            |
+| Tile               | 6~12   | &check;                     | &check;            |
+| TopK               | 11~16  | &check;                     | &check;            |
+| Transpose          | 1~12   | &check;                     | &check;            |
+| Unsqueeze          | 11~12  | &check;                     | &check;            |
+| Where              | 9~15   | &check;                     | &check;            |
 
 * MMCV
 
-| Op Type           | Op Set | Linux X86-64 | Linux CUDA |
-|:-----------------:|:------:|:------------:|:----------:|
-| NonMaxSuppression | 1      | &check;      | &check;    |
-| RoiAlign          | 1      | &check;      | &check;    |
-| grid_sample       | 1      | &check;      | &check;    |
+| Op Type           | Op Set | Linux/Windows/Darwin X86-64 | Linux/Windows CUDA |
+|:-----------------:|:------:|:---------------------------:|:------------------:|
+| NonMaxSuppression | 1      | &check;                     | &check;            |
+| RoiAlign          | 1      | &check;                     | &check;            |
+| grid_sample       | 1      | &check;                     | &check;            |
 
 * PPL
 
-| Op Type        | Op Set | Linux X86-64 | Linux CUDA |
-|:--------------:|:------:|:------------:|:----------:|
-| ChannelShuffle | 1      | &check;      | &check;    |
-| [ShapeOperation](shape_operation.md)        | 1      | &check;      | &check;    |
-| Swish          | 1      | &check;      |            |
+| Op Type                              | Op Set | Linux/Windows/Darwin X86-64 | Linux/Windows CUDA |
+|:------------------------------------:|:------:|:---------------------------:|:------------------:|
+| ChannelShuffle                       | 1      | &check;                     | &check;            |
+| [ShapeOperation](shape_operation.md) | 1      | &check;                     | &check;            |
+| Swish                                | 1      | &check;                     |                    |
--- src/ppl/nn/engines/cuda/optimizer/opt_kernel_creator_manager.cc
+++ src/ppl/nn/engines/cuda/optimizer/opt_kernel_creator_manager.cc
@@ -106,7 +106,11 @@ RetCode OptKernelCreatorManager::Register(const string& domain, const string& ty
 }
 
 OptKernelCreator OptKernelCreatorManager::Find(const string& domain, const string& type, uint64_t version) const {
-    return *mgr_.Find(domain, type, version);
+    auto ret = mgr_.Find(domain, type, version);
+    if (ret) {
+        return *ret;
+    }
+    return nullptr;
 }
 
 void OptKernelCreatorManager::Remove(const string& domain, const string& type) {
@@ -118,92 +122,121 @@ static CudaOptKernel* GenericCreateOptKernel(const ir::Node* node) {
     return new T(node);
 }
 
-#define REGISTER_OPT_KERNEL_CREATOR(domain, type, first_version, last_version, classname) \
-    Register(domain, type, utils::VersionRange(first_version, last_version), GenericCreateOptKernel<classname>)
+#define REGISTER_OPT_KERNEL_CREATOR(domain, type, first_version, last_version, classname)                      \
+    do {                                                                                                       \
+        if (last_version < first_version) {                                                                    \
+            LOG(ERROR) << "register op[" << domain << ":" << type << "] failed: last_version[" << last_version \
+                       << "] < first_version[" << first_version << "]";                                        \
+            exit(-1);                                                                                          \
+        }                                                                                                      \
+        auto status = Register(domain, type, utils::VersionRange(first_version, last_version),                 \
+                               GenericCreateOptKernel<classname>);                                             \
+        if (status != RC_SUCCESS) {                                                                            \
+            exit(-1);                                                                                          \
+        }                                                                                                      \
+    } while (0)
 
+// NOTE: sorted in alphabet order
 OptKernelCreatorManager::OptKernelCreatorManager() {
     // onnx op's default domain is ""
-    REGISTER_OPT_KERNEL_CREATOR("", "Add", 11, 11, AddOp);
+    // A
+    REGISTER_OPT_KERNEL_CREATOR("", "Add", 7, 12, AddOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "And", 7, 16, AndOp);
     REGISTER_OPT_KERNEL_CREATOR("", "ArgMax", 11, 11, ArgmaxOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "AveragePool", 11, 11, AveragePoolOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "GlobalAveragePool", 11, 11, AveragePoolOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "AveragePool", 11, 16, AveragePoolOp);
+    // B
+    REGISTER_OPT_KERNEL_CREATOR("", "BatchNormalization", 9, 13, BatchNormalizationOp);
+    // C
+    REGISTER_OPT_KERNEL_CREATOR("", "Cast", 9, 12, CastOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Ceil", 6, 12, CeilOp);
     REGISTER_OPT_KERNEL_CREATOR("", "Clip", 11, 11, ClipOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Concat", 11, 11, ConcatOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Conv", 11, 11, ConvOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Div", 11, 11, DivOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Equal", 11, 11, EqualOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Exp", 11, 11, ExpOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Expand", 11, 11, ExpandOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Flatten", 11, 11, FlattenOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Concat", 11, 12, ConcatOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "ConstantOfShape", 9, 16, ConstantOfShapeOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Conv", 11, 16, ConvOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "ConvTranspose", 11, 16, ConvTransposeOp);
+    // D
+    REGISTER_OPT_KERNEL_CREATOR("", "DepthToSpace", 11, 12, DepthToSpaceOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Div", 7, 12, DivOp);
+    // E
+    REGISTER_OPT_KERNEL_CREATOR("", "Equal", 11, 12, EqualOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Exp", 6, 12, ExpOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Expand", 8, 12, ExpandOp);
+    // F
+    REGISTER_OPT_KERNEL_CREATOR("", "Flatten", 11, 12, FlattenOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Floor", 6, 12, FloorOp);
+    // G
+    REGISTER_OPT_KERNEL_CREATOR("", "Gather", 11, 12, GatherOp);
     REGISTER_OPT_KERNEL_CREATOR("", "GatherND", 11, 11, GatherNDOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Gather", 11, 11, GatherOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Gemm", 11, 11, GemmOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Greater", 11, 11, GreaterOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Identity", 11, 11, IdentityOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "LeakyRelu", 11, 11, LeakyReluOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Max", 11, 11, MaxOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Min", 11, 11, MinOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Gemm", 11, 12, GemmOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "GlobalAveragePool", 1, 16, AveragePoolOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Greater", 9, 12, GreaterOp);
+    // I
+    REGISTER_OPT_KERNEL_CREATOR("", "Identity", 1, 12, IdentityOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "If", 11, 12, IfOp);
+    // L
+    REGISTER_OPT_KERNEL_CREATOR("", "LeakyRelu", 6, 16, LeakyReluOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Less", 9, 12, LessOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Log", 6, 12, LogOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Loop", 11, 12, LoopOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "LSTM", 7, 13, LstmOp);
+    // M
+    REGISTER_OPT_KERNEL_CREATOR("", "MatMul", 9, 12, MatMulOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Max", 8, 11, MaxOp);
     REGISTER_OPT_KERNEL_CREATOR("", "MaxPool", 11, 11, MaxPoolOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "MaxUnpool", 11, 11, MaxUnPoolOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "MatMul", 11, 11, MatMulOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Mul", 11, 11, MulOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "NonMaxSuppression", 11, 11, NonMaxSupressionOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "NonZero", 11, 11, NonZeroOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Not", 11, 11, NotOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Pad", 11, 11, PadOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "MaxUnpool", 11, 16, MaxUnPoolOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Min", 8, 11, MinOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Mul", 7, 12, MulOp);
+    // N
+    REGISTER_OPT_KERNEL_CREATOR("", "NonMaxSuppression", 11, 16, NonMaxSupressionOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "NonZero", 9, 12, NonZeroOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Not", 1, 16, NotOp);
+    // P
+    REGISTER_OPT_KERNEL_CREATOR("", "Pad", 11, 12, PadOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Pow", 7, 11, PowOp);
+    // R
+    REGISTER_OPT_KERNEL_CREATOR("", "Range", 11, 16, RangeOp);
     REGISTER_OPT_KERNEL_CREATOR("", "ReduceMax", 11, 11, ReduceMaxOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "ReduceMean", 11, 12, ReduceMeanOp);
     REGISTER_OPT_KERNEL_CREATOR("", "ReduceMin", 11, 11, ReduceMinOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "ReduceSum", 11, 11, ReduceSumOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "ReduceMean", 11, 11, ReduceMeanOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Relu", 11, 11, ReluOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Reshape", 11, 11, ReshapeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "ScatterND", 11, 11, ScatterNDOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Sigmoid", 11, 11, SigmoidOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Softmax", 11, 11, SoftmaxOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Split", 11, 11, SplitOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Sqrt", 11, 11, SqrtOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Squeeze", 11, 11, SqueezeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Sub", 11, 11, SubOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Transpose", 11, 11, TransposeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Unsqueeze", 11, 11, UnsqueezeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Cast", 11, 11, CastOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Pow", 11, 11, PowOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Slice", 11, 11, SliceOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "BatchNormalization", 11, 11, BatchNormalizationOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Where", 11, 11, WhereOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Range", 11, 11, RangeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "TopK", 11, 11, TopKOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Resize", 11, 11, ResizeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "RoiAlign", 11, 11, ROIAlignOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Shape", 11, 11, ShapeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "ConstantOfShape", 11, 11, ConstantOfShapeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Log", 11, 11, LogOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "DepthToSpace", 11, 11, DepthToSpaceOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Tile", 11, 11, TileOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Less", 11, 11, LessOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Floor", 11, 11, FloorOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "ConvTranspose", 11, 11, ConvTransposeOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Tanh", 11, 11, TanhOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "ScatterElements", 11, 11, ScatterElementsOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Ceil", 11, 11, CeilOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "And", 11, 11, AndOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "ReduceProd", 11, 11, ReduceProdOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "If", 1, 13, IfOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "Loop", 1, 13, LoopOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "SplitToSequence", 1, 13, SplitToSequenceOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "SequenceAt", 1, 13, SequenceAtOp);
-    REGISTER_OPT_KERNEL_CREATOR("", "LSTM", 11, 11, LstmOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "ReduceProd", 11, 12, ReduceProdOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "ReduceSum", 11, 12, ReduceSumOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Relu", 6, 12, ReluOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Reshape", 5, 12, ReshapeOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Resize", 11, 12, ResizeOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "RoiAlign", 10, 15, ROIAlignOp);
+    // S
+    REGISTER_OPT_KERNEL_CREATOR("", "ScatterElements", 11, 12, ScatterElementsOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "ScatterND", 11, 12, ScatterNDOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "SequenceAt", 11, 16, SequenceAtOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Shape", 1, 12, ShapeOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Sigmoid", 6, 12, SigmoidOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Slice", 11, 12, SliceOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Softmax", 11, 12, SoftmaxOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Split", 11, 12, SplitOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "SplitToSequence", 11, 16, SplitToSequenceOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Sqrt", 6, 12, SqrtOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Squeeze", 11, 12, SqueezeOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Sub", 7, 12, SubOp);
+    // T
+    REGISTER_OPT_KERNEL_CREATOR("", "Tanh", 6, 12, TanhOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Tile", 6, 12, TileOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "TopK", 11, 16, TopKOp);
+    REGISTER_OPT_KERNEL_CREATOR("", "Transpose", 1, 12, TransposeOp);
+    // U
+    REGISTER_OPT_KERNEL_CREATOR("", "Unsqueeze", 11, 12, UnsqueezeOp);
+    // W
+    REGISTER_OPT_KERNEL_CREATOR("", "Where", 9, 15, WhereOp);
 
     // mmcv op domain is "mmcv"
-    REGISTER_OPT_KERNEL_CREATOR("mmcv", "NonMaxSuppression", 1, 1, MMCVNonMaxSupressionOp);
-    REGISTER_OPT_KERNEL_CREATOR("mmcv", "MMCVRoiAlign", 1, 1, MMCVROIAlignOp);
     REGISTER_OPT_KERNEL_CREATOR("mmcv", "grid_sampler", 1, 1, MMCVGridSampleOp);
+    REGISTER_OPT_KERNEL_CREATOR("mmcv", "MMCVRoiAlign", 1, 1, MMCVROIAlignOp);
     REGISTER_OPT_KERNEL_CREATOR("mmcv", "MMCVModulatedDeformConv2d", 1, 1, MMCVModulatedDeformConv2dOp);
+    REGISTER_OPT_KERNEL_CREATOR("mmcv", "NonMaxSuppression", 1, 1, MMCVNonMaxSupressionOp);
 
     // ppl customize op domain is "ppl"
     REGISTER_OPT_KERNEL_CREATOR("ppl", "Bridge", 1, 1, BridgeOp);
-    REGISTER_OPT_KERNEL_CREATOR("ppl", "Shape", 1, 1, PPLShapeOperationOp);
     REGISTER_OPT_KERNEL_CREATOR("ppl", "ChannelShuffle", 1, 1, ChannelShuffleOp);
+    REGISTER_OPT_KERNEL_CREATOR("ppl", "Shape", 1, 1, PPLShapeOperationOp);
 }
 
 }}} // namespace ppl::nn::cuda
