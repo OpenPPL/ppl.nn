ce2de4c4476e32fef046db13700855d7e674184b
ouguoyu <ouguoyu@sensetime.com>
2021-11-02 19:54:39 +0800

    [build] use cmake `--build` to build and install targets

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 md pplnn-build
 cd pplnn-build
-cmake %* -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%cd%/install ..
-cmake --build . --config Release
-cmake --install . --prefix %cd%/install
+cmake %* -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install ..
+cmake --build . -j --config Release
+cmake --build . --target install -j --config Release
 cd ..
