08bc75e0be116705f2e15bf7176839e679b9337d
ouguoyu <ouguoyu@sensetime.com>
2022-01-05 16:30:28 +0800

    [fix] specified build type in build.sh

--- build.sh
+++ build.sh
@@ -10,11 +10,12 @@ else
     processor_num=1
 fi
 
-options="-DCMAKE_INSTALL_PREFIX=install $*"
+build_type='RelWithDebInfo'
+options="-DCMAKE_BUILD_TYPE=${build_type} -DCMAKE_INSTALL_PREFIX=install $*"
 
 pplnn_build_dir="${workdir}/pplnn-build"
 mkdir ${pplnn_build_dir}
 cd ${pplnn_build_dir}
-cmd="cmake $options .. && cmake --build . -j ${processor_num} --config Release && cmake --build . --target install -j ${processor_num} --config Release"
+cmd="cmake $options .. && cmake --build . -j ${processor_num} --config ${build_type} && cmake --build . --target install -j ${processor_num} --config ${build_type}"
 echo "cmd -> $cmd"
 eval "$cmd"
