a81409389a983090630ae766ac842b4538c4f295
alcanderian <alcanderian@gmail.com>
2022-01-18 20:28:11 +0800

    [x86][fix] fix avx512 compile

--- src/ppl/nn/engines/x86/impls/include/ppl/kernel/x86/fp32/softmax.h
+++ src/ppl/nn/engines/x86/impls/include/ppl/kernel/x86/fp32/softmax.h
@@ -22,11 +22,13 @@
 
 namespace ppl { namespace kernel { namespace x86 {
 
+#ifdef PPL_USE_X86_AVX512
 ppl::common::RetCode softmax_ndarray_fp32_avx512(
     const ppl::nn::TensorShape *src_shape,
     const float *src,
     const int64_t axis,
     float *dst);
+#endif
 
 ppl::common::RetCode softmax_ndarray_fp32_fma(
     const ppl::nn::TensorShape *src_shape,
