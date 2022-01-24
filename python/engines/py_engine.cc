116c7940e18c1a05a782a0abb0774d120190fb50
ouguoyu <ouguoyu@sensetime.com>
2022-01-24 18:00:24 +0800

    [refactor] use shared_ptr to initialize PyEngine so that external engines can register to nn module

--- python/engines/py_engine.cc
+++ python/engines/py_engine.cc
@@ -23,15 +23,7 @@ namespace ppl { namespace nn { namespace python {
 
 void RegisterEngine(pybind11::module* m) {
     pybind11::class_<PyEngine>(*m, "Engine")
-#ifdef PPLNN_USE_X86
-        .def(pybind11::init<PyX86Engine>())
-#endif
-#ifdef PPLNN_USE_CUDA
-        .def(pybind11::init<PyCudaEngine>())
-#endif
-#ifdef PPLNN_USE_RISCV
-        .def(pybind11::init<PyRiscvEngine>())
-#endif
+        .def(pybind11::init<std::shared_ptr<Engine>>())
         .def("__bool__", [](const PyEngine& engine) -> bool {
             return (engine.ptr.get());
         });
