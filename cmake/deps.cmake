59d894b7d097d34b5be76d8bbc4a2f26edae0a07
ouguoyu <ouguoyu@sensetime.com>
2022-05-10 21:46:03 +0800

    [build] deps can be downloaded/cloned from urls specified by users

--- cmake/deps.cmake
+++ cmake/deps.cmake
@@ -45,12 +45,30 @@ if(NOT Git_FOUND)
     message(FATAL_ERROR "git is required.")
 endif()
 
-FetchContent_Declare(hpcc
-    GIT_REPOSITORY https://github.com/openppl-public/hpcc.git
-    GIT_TAG cab69f864c3ce185ab9c4d69fcd2aa17e6eaf34d
-    SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
-    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
-    SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild)
+set(__HPCC_COMMIT__ d3302ce75b8bca78f25c770517e99d29a16c8ccb)
+
+if(PPLNN_DEP_HPCC_PKG)
+    FetchContent_Declare(hpcc
+        URL ${PPLNN_DEP_HPCC_PKG}
+        SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
+        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
+        SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild)
+elseif(PPLNN_DEP_HPCC_GIT)
+    FetchContent_Declare(hpcc
+        GIT_REPOSITORY ${PPLNN_DEP_HPCC_GIT}
+        GIT_TAG ${__HPCC_COMMIT__}
+        SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
+        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
+        SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild)
+else()
+    FetchContent_Declare(hpcc
+        URL https://github.com/openppl-public/hpcc/archive/${__HPCC_COMMIT__}.zip
+        SOURCE_DIR ${HPCC_DEPS_DIR}/hpcc
+        BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/hpcc-build
+        SUBBUILD_DIR ${HPCC_DEPS_DIR}/hpcc-subbuild)
+endif()
+
+unset(__HPCC_COMMIT__)
 
 FetchContent_GetProperties(hpcc)
 if(NOT hpcc_POPULATED)
@@ -66,27 +84,63 @@ set(PPLCOMMON_ENABLE_PYTHON_API ${PPLNN_ENABLE_PYTHON_API})
 set(PPLCOMMON_ENABLE_LUA_API ${PPLNN_ENABLE_LUA_API})
 set(PPLCOMMON_HOLD_DEPS ${PPLNN_HOLD_DEPS})
 
-hpcc_declare_git_dep(pplcommon
-    https://github.com/openppl-public/ppl.common.git
-    4c54cc4d6781cd8f7170be70c1f2af93429588c6)
+set(__PPLCOMMON_COMMIT__ 4c54cc4d6781cd8f7170be70c1f2af93429588c6)
+
+if(PPLNN_DEP_PPLCOMMON_PKG)
+    hpcc_declare_pkg_dep(pplcommon
+        ${PPLNN_DEP_PPLCOMMON_PKG})
+elseif(PPLNN_DEP_PPLCOMMON_GIT)
+    hpcc_declare_git_dep(pplcommon
+        ${PPLNN_DEP_PPLCOMMON_GIT}
+        ${__PPLCOMMON_COMMIT__})
+else()
+    hpcc_declare_pkg_dep(pplcommon
+        https://github.com/openppl-public/ppl.common/archive/${__PPLCOMMON_COMMIT__}.zip)
+endif()
+
+unset(__PPLCOMMON_COMMIT__)
 
 # --------------------------------------------------------------------------- #
 
 set(FLATBUFFERS_BUILD_TESTS OFF CACHE BOOL "disable tests")
 set(FLATBUFFERS_INSTALL OFF CACHE BOOL "disable installation")
 
-hpcc_declare_pkg_dep(flatbuffers
-    https://github.com/google/flatbuffers/archive/refs/tags/v2.0.0.zip
-    9e5d08df19d5ff127c352d1466ce58a7)
+set(__FLATBUFFERS_TAG__ v2.0.0)
+
+if(PPLNN_DEP_FLATBUFFERS_PKG)
+    hpcc_declare_pkg_dep(flatbuffers
+        ${PPLNN_DEP_FLATBUFFERS_PKG})
+elseif(PPLNN_DEP_FLATBUFFERS_GIT)
+    hpcc_declare_git_dep(flatbuffers
+        ${PPLNN_DEP_FLATBUFFERS_GIT}
+        ${__FLATBUFFERS_TAG__})
+else()
+    hpcc_declare_pkg_dep(flatbuffers
+        https://github.com/google/flatbuffers/archive/refs/tags/${__FLATBUFFERS_TAG__}.zip)
+endif()
+
+unset(__FLATBUFFERS_TAG__)
 
 # --------------------------------------------------------------------------- #
 
 set(protobuf_BUILD_TESTS OFF CACHE BOOL "disable protobuf tests")
 set(protobuf_BUILD_PROTOC_BINARIES OFF CACHE BOOL "")
 
-hpcc_declare_pkg_dep(protobuf
-    https://github.com/protocolbuffers/protobuf/releases/download/v3.12.4/protobuf-cpp-3.12.4.zip
-    41651ed9f6cd35063f57003ff24bacfb)
+set(__PROTOBUF_TAG__ v3.12.4)
+
+if(PPLNN_DEP_PROTOBUF_PKG)
+    hpcc_declare_pkg_dep(protobuf
+        ${PPLNN_DEP_PROTOBUF_PKG})
+elseif(PPLNN_DEP_PROTOBUF_GIT)
+    hpcc_declare_git_dep(protobuf
+        ${PPLNN_DEP_PROTOBUF_GIT}
+        ${__PROTOBUF_TAG__})
+else()
+    hpcc_declare_pkg_dep(protobuf
+        https://github.com/protocolbuffers/protobuf/releases/download/${__PROTOBUF_TAG__}/protobuf-cpp-3.12.4.zip)
+endif()
+
+unset(__PROTOBUF_TAG__)
 
 # --------------------------------------------------------------------------- #
 
@@ -94,9 +148,21 @@ set(RAPIDJSON_BUILD_TESTS OFF CACHE BOOL "disable rapidjson tests")
 set(RAPIDJSON_BUILD_EXAMPLES OFF CACHE BOOL "disable rapidjson examples")
 set(RAPIDJSON_BUILD_DOC OFF CACHE BOOL "disable rapidjson docs")
 
-hpcc_declare_pkg_dep(rapidjson
-    https://github.com/Tencent/rapidjson/archive/2e8f5d897d9d461a7273b4b812b0127f321b1dcf.zip
-    aadb4462dab0f019a5522ae4489ee1aa)
+set(__RAPIDJSON_COMMIT__ 2e8f5d897d9d461a7273b4b812b0127f321b1dcf)
+
+if(PPLNN_DEP_RAPIDJSON_PKG)
+    hpcc_declare_pkg_dep(rapidjson
+        ${PPLNN_DEP_RAPIDJSON_PKG})
+elseif(PPLNN_DEP_RAPIDJSON_GIT)
+    hpcc_declare_git_dep(rapidjson
+        ${PPLNN_DEP_RAPIDJSON_GIT}
+        ${__RAPIDJSON_COMMIT__})
+else()
+    hpcc_declare_pkg_dep(rapidjson
+        https://github.com/Tencent/rapidjson/archive/${__RAPIDJSON_COMMIT__}.zip)
+endif()
+
+unset(__RAPIDJSON_COMMIT__)
 
 # --------------------------------------------------------------------------- #
 
@@ -105,18 +171,42 @@ set(PYBIND11_TEST OFF CACHE BOOL "disable pybind11 tests")
 set(PYBIND11_NOPYTHON ON CACHE BOOL "do not find python")
 set(PYBIND11_FINDPYTHON OFF CACHE BOOL "do not find python")
 
-hpcc_declare_pkg_dep(pybind11
-    https://github.com/pybind/pybind11/archive/refs/tags/v2.9.2.zip
-    399d924015f477723ddedbf6f044ab5b)
+set(__PYBIND11_TAG__ v2.9.2)
+
+if(PPLNN_DEP_PYBIND11_PKG)
+    hpcc_declare_pkg_dep(pybind11
+        ${PPLNN_DEP_PYBIND11_PKG})
+elseif(PPLNN_DEP_PYBIND11_GIT)
+    hpcc_declare_git_dep(pybind11
+        ${PPLNN_DEP_PYBIND11_GIT}
+        ${__PYBIND11_TAG__})
+else()
+    hpcc_declare_pkg_dep(pybind11
+        https://github.com/pybind/pybind11/archive/refs/tags/${__PYBIND11_TAG__}.zip)
+endif()
+
+unset(__PYBIND11_TAG__)
 
 # --------------------------------------------------------------------------- #
 
 set(LUACPP_INSTALL OFF CACHE BOOL "")
 set(LUACPP_BUILD_TESTS OFF CACHE BOOL "")
 
-hpcc_declare_pkg_dep(luacpp
-    https://github.com/ouonline/lua-cpp/archive/d79c270eee56646f22eba76c71f1ff1c0e5f9ac6.zip
-    c067bcb0618f179867d293e179f75ffc)
+set(__LUACPP_COMMIT__ d79c270eee56646f22eba76c71f1ff1c0e5f9ac6)
+
+if(PPLNN_DEP_LUACPP_PKG)
+    hpcc_declare_pkg_dep(luacpp
+        ${PPLNN_DEP_LUACPP_PKG})
+elseif(PPLNN_DEP_LUACPP_GIT)
+    hpcc_declare_git_dep(luacpp
+        ${PPLNN_DEP_LUACPP_GIT}
+        ${__LUACPP_COMMIT__})
+else()
+    hpcc_declare_pkg_dep(luacpp
+        https://github.com/ouonline/lua-cpp/archive/${__LUACPP_COMMIT__}.zip)
+endif()
+
+unset(__LUACPP_COMMIT__)
 
 # --------------------------------------------------------------------------- #
 
@@ -124,6 +214,18 @@ set(BUILD_GMOCK OFF CACHE BOOL "")
 set(INSTALL_GTEST OFF CACHE BOOL "")
 set(BUILD_SHARED_LIBS OFF CACHE BOOL "")
 
-hpcc_declare_pkg_dep(googletest
-    https://github.com/google/googletest/archive/refs/tags/release-1.10.0.zip
-    82358affdd7ab94854c8ee73a180fc53)
+set(__GOOGLETEST_TAG__ release-1.10.0)
+
+if(PPLNN_DEP_GOOGLETEST_PKG)
+    hpcc_declare_pkg_dep(googletest
+        ${PPLNN_DEP_GOOGLETEST_PKG})
+elseif(PPLNN_DEP_GOOGLETEST_GIT)
+    hpcc_declare_git_dep(googletest
+        ${PPLNN_DEP_GOOGLETEST_GIT}
+        ${__GOOGLETEST_TAG__})
+else()
+    hpcc_declare_pkg_dep(googletest
+        https://github.com/google/googletest/archive/refs/tags/${__GOOGLETEST_TAG__}.zip)
+endif()
+
+unset(__GOOGLETEST_TAG__)
