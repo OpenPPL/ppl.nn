1237e2069e91e8feed7b208406163bbe45d1f114
ouguoyu <ouguoyu@sensetime.com>
2022-05-12 14:16:25 +0800

    [feature] export PPLNN_VERSION_STR and PPLNN_COMMIT_STR to get version info

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -1,6 +1,12 @@
-file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION __PPLNN_VERSION_STR__)
-string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" __PPLNN__UNUSED__ ${__PPLNN_VERSION_STR__})
+# these variables can be set to the internal value when plugins are embedded into pplnn_static
+#   - PPLNN_VERSION_STR: <major>.<minor>.<patch>
+#   - PPLNN_COMMIT_HASH: a string
 
+if(NOT PPLNN_VERSION_STR)
+    file(READ ${CMAKE_CURRENT_SOURCE_DIR}/VERSION PPLNN_VERSION_STR)
+endif()
+
+string(REGEX MATCH "([0-9]+)\\.([0-9]+)\\.([0-9]+)" __PPLNN__UNUSED__ ${PPLNN_VERSION_STR})
 set(PPLNN_VERSION_MAJOR ${CMAKE_MATCH_1})
 set(PPLNN_VERSION_MINOR ${CMAKE_MATCH_2})
 set(PPLNN_VERSION_PATCH ${CMAKE_MATCH_3})
@@ -10,9 +16,9 @@ list(APPEND PPLNN_COMPILE_DEFINITIONS
     PPLNN_VERSION_MINOR=${PPLNN_VERSION_MINOR}
     PPLNN_VERSION_PATCH=${PPLNN_VERSION_PATCH})
 
-hpcc_get_git_info(
-    GIT_HASH_OUTPUT PPLNN_COMMIT_HASH
-    GIT_TAG_OUTPUT PPLNN_COMMIT_TAG)
+if(NOT PPLNN_COMMIT_STR)
+    hpcc_get_git_info(GIT_HASH_OUTPUT PPLNN_COMMIT_STR)
+endif()
 
 set(__PPLNN_COMMIT_ID_SRC__ ${CMAKE_CURRENT_BINARY_DIR}/generated/commit.cc)
 
@@ -25,4 +31,3 @@ list(APPEND PPLNN_SOURCES ${__PPLNN_COMMIT_ID_SRC__})
 
 unset(__PPLNN_COMMIT_ID_SRC__)
 unset(__PPLNN__UNUSED__)
-unset(__PPLNN_VERSION_STR__)
