02418bb57bef2d888b57d44589a599080cb806d9
ouguoyu <ouguoyu@sensetime.com>
2022-07-06 00:52:43 +0800

    [misc] install config file minor changes

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,11 +1,8 @@
 install(TARGETS pplnn_static DESTINATION lib)
 
-set(__PPLNN_CMAKE_CONFIG_FILE__ ${CMAKE_CURRENT_BINARY_DIR}/generated/pplnn-config.cmake)
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cmake/pplnn-config.cmake.in
-    ${__PPLNN_CMAKE_CONFIG_FILE__}
+    ${CMAKE_INSTALL_PREFIX}/lib/cmake/ppl/pplnn-config.cmake
     @ONLY)
-install(FILES ${__PPLNN_CMAKE_CONFIG_FILE__} DESTINATION lib/cmake/ppl)
-unset(__PPLNN_CMAKE_CONFIG_FILE__)
 
 install(DIRECTORY include/ppl/nn/common DESTINATION include/ppl/nn)
 install(DIRECTORY include/ppl/nn/runtime DESTINATION include/ppl/nn)
