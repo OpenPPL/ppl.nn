b08ca1ea7093fcf50933f46c6bf1e9fbb43db9f8
ouguoyu <ouguoyu@sensetime.com>
2022-07-12 12:04:09 +0800

    [misc] reuse `TensorImpl` in `InferShapes()`

--- src/ppl/nn/runtime/scheduler_common.cc
+++ src/ppl/nn/runtime/scheduler_common.cc
@@ -22,8 +22,8 @@ using namespace ppl::common;
 
 namespace ppl { namespace nn { namespace utils {
 
-static RetCode AfterExecuteKernel(KernelImpl* kernel, KernelExecContext* ctx,
-                                  const function<RetCode(EdgeObject*, nodeid_t)>& release_func) {
+RetCode ReleaseKernelInputOutput(KernelImpl* kernel, InputOutputInfo* ctx,
+                                 const function<RetCode(EdgeObject*, nodeid_t)>& release_func) {
     auto nid = kernel->GetNode()->GetId();
 
     for (uint32_t i = 0; i < ctx->GetInputCount(); ++i) {
@@ -62,28 +62,6 @@ static RetCode AfterExecuteKernel(KernelImpl* kernel, KernelExecContext* ctx,
     }
 
     return RC_SUCCESS;
-}
-
-RetCode ExecuteKernel(KernelImpl* kernel, KernelExecContext* ctx,
-                      const function<RetCode(EdgeObject*, nodeid_t)>& release_func, Profiler* profiler) {
-    auto exec_status = kernel->Execute(ctx);
-
-#ifdef PPLNN_ENABLE_KERNEL_PROFILING
-    if (profiler) {
-        profiler->CollectStatistics(kernel);
-    }
-#endif
-
-    auto status = AfterExecuteKernel(kernel, ctx, release_func);
-
-    if (exec_status != RC_SUCCESS) {
-        auto& type = kernel->GetNode()->GetType();
-        LOG(ERROR) << "exec kernel[" << kernel->GetName() << "] of type[" << type.domain << ":" << type.name << ":"
-                   << type.version << "] failed: " << GetRetCodeStr(exec_status);
-        return exec_status;
-    }
-
-    return status;
 }
 
 }}} // namespace ppl::nn::utils
