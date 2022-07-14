98a1bc130aa4b32977bc5313560ad452935dc8ae
ouguoyu <ouguoyu@sensetime.com>
2022-07-15 01:03:09 +0800

    [build][cuda] add embedding sourcee support and unset local cmake variables

--- cmake/cuda.cmake
+++ cmake/cuda.cmake
@@ -1,7 +1,5 @@
 option(PPLNN_ENABLE_CUDA_JIT "enable cuda JIT support" ON)
 
-# ----- required cuda version >= 10.2 ----- #
-
 include(${HPCC_DEPS_DIR}/hpcc/cmake/cuda-common.cmake)
 
 if(CUDA_VERSION VERSION_LESS "9.0")
@@ -16,8 +14,9 @@ if(PPLNN_USE_MSVC_STATIC_RUNTIME)
     hpcc_cuda_use_msvc_static_runtime()
 endif()
 
-file(GLOB_RECURSE PPLNN_CUDA_SRC src/ppl/nn/engines/cuda/*.cc)
-list(APPEND PPLNN_SOURCES ${PPLNN_CUDA_SRC})
+file(GLOB_RECURSE __PPLNN_CUDA_SRC__ src/ppl/nn/engines/cuda/*.cc)
+list(APPEND PPLNN_SOURCES ${__PPLNN_CUDA_SRC__})
+unset(__PPLNN_CUDA_SRC__)
 
 add_subdirectory(src/ppl/nn/engines/cuda/impls)
 list(APPEND PPLNN_LINK_LIBRARIES pplkernelcuda_static)
