194a9181ac620afff399028ac8453d4372742186
ouguoyu <ouguoyu@sensetime.com>
2022-07-15 10:40:02 +0800

    [build] eliminate .. in cmake path

--- src/ppl/nn/engines/riscv/impls/pplkernelriscv-config.cmake
+++ src/ppl/nn/engines/riscv/impls/pplkernelriscv-config.cmake
@@ -4,18 +4,16 @@ if(TARGET "pplkernelriscv_static")
     return()
 endif()
 
-set(__PPLKERNELRISCV_PACKAGE_DIR__ "${CMAKE_CURRENT_LIST_DIR}/../../..")
-
 add_library(pplkernelriscv_static STATIC IMPORTED)
 
 if(NOT TARGET "pplcommon_static")
     include(${CMAKE_CURRENT_LIST_DIR}/pplcommon-config.cmake)
 endif()
 
+get_filename_component(__PPLKERNELRISCV_PACKAGE_DIR__ "${CMAKE_CURRENT_LIST_DIR}/../../.." ABSOLUTE)
 set_target_properties(pplkernelriscv_static PROPERTIES
     INTERFACE_LINK_LIBRARIES "pplcommon_static"
     IMPORTED_LOCATION "${__PPLKERNELRISCV_PACKAGE_DIR__}/lib/libpplkernelriscv_static.a"
     IMPORTED_LOCATION_DEBUG "${__PPLKERNELRISCV_PACKAGE_DIR__}/lib/libpplkernelriscv_static.a"
     IMPORTED_LOCATION_RELEASE "${__PPLKERNELRISCV_PACKAGE_DIR__}/lib/libpplkernelriscv_static.a")
-
 unset(__PPLKERNELRISCV_PACKAGE_DIR__)
