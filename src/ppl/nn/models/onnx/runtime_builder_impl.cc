cb0288099e8e3cf23f9b870447c1b94aa7ea7bca
ouguoyu <ouguoyu@sensetime.com>
2022-09-16 21:23:22 +0800

    [misc] print error message of mapping file failure

--- src/ppl/nn/models/onnx/runtime_builder_impl.cc
+++ src/ppl/nn/models/onnx/runtime_builder_impl.cc
@@ -56,7 +56,7 @@ RetCode RuntimeBuilderImpl::LoadModel(const char* model_file) {
     FileMapping fm;
     auto status = fm.Init(model_file, FileMapping::READ);
     if (status != RC_SUCCESS) {
-        LOG(ERROR) << "init filemapping from file [" << model_file << "] faild: " << GetRetCodeStr(status);
+        LOG(ERROR) << "mapping file [" << model_file << "] faild: " << fm.GetErrorMessage();
         return status;
     }
 
