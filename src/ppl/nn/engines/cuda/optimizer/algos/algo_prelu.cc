c471ffeaa4c3dd8359c506bfa228b7f2e3ffb050
wangjianfei <wangjianfei@sensetime.com>
2022-10-18 23:32:09 -0400

    [cuda][add]: prelu with inherited formats

---
+++ src/ppl/nn/engines/cuda/optimizer/algos/algo_prelu.cc
@@ -0,0 +1,62 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "ppl/nn/engines/cuda/optimizer/algos/algo_prelu.h"
+
+#include <chrono>
+
+#include "ppl/common/cuda/cuda_types.h"
+#include "cudakernel/nn/prelu.h"
+#include "ppl/nn/common/logger.h"
+#include "ppl/nn/utils/utils.h"
+
+using namespace ppl::common;
+
+namespace ppl { namespace nn { namespace cuda {
+
+double PReluAlgorithm::ExcuteTimer(const ir::Node* node, OptKernelOptions& options) {
+    return 1e-5f;
+}
+
+RetCode PReluAlgorithm::ModifyParam(ir::Node* node, OptKernelOptions& options) {
+    return RC_SUCCESS;
+}
+
+void PReluAlgorithm::ReshapeOnEdges(const ir::Node* node, std::map<edgeid_t, std::unique_ptr<TensorImpl>>* tensors,
+                                   dataformat_t input_format, dataformat_t output_format) {
+    for (uint32_t i = 0; i < node->GetInputCount(); ++i) { // only reset formats of input0
+        auto edge_id = node->GetInput(i);
+        if (edge_id == INVALID_EDGEID) {
+            continue;
+        }
+        auto shape = tensors->find(edge_id)->second->GetShape();
+        if (i == 0 && shape->GetDimCount() > 1) {
+            shape->SetDataFormat(input_format);
+        } else {
+            shape->SetDataFormat(DATAFORMAT_NDARRAY);
+        }
+    }
+
+    for (uint32_t i = 0; i < node->GetOutputCount(); ++i) {
+        auto edge_id = node->GetOutput(i);
+        auto shape = tensors->find(edge_id)->second->GetShape();
+        shape->SetDataFormat(output_format);
+    }
+    return;
+}
+
+}}} // namespace ppl::nn::cuda
