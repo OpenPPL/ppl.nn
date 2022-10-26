d295acce30da1e8d54f320e675ae91ffdce0d2bc
wangjianfei <wangjianfei@sensetime.com>
2022-10-26 04:56:55 -0400

    [cuda][add]: conv and deconv support dynamic weight

--- src/ppl/nn/engines/cuda/kernels/onnx/convtranspose_kernel.cc
+++ src/ppl/nn/engines/cuda/kernels/onnx/convtranspose_kernel.cc
@@ -22,6 +22,19 @@
 
 namespace ppl { namespace nn { namespace cuda {
 
+#define ALGO_MAX_TIME (3.0e+10)
+
+#define ALLOC_BUFFERF_FOR_ALGO_SELECT(___buffer_name___, ___size___, ___ret___)                  \
+    BufferDesc ___buffer_name___;  \
+    status =  GetCudaDevice()->ReallocWithRandomValue(___size___, &___buffer_name___);             \
+    if (status != RC_SUCCESS) {                                                                  \
+        LOG(DEBUG) << "alloc " #___buffer_name___ " tensor failed";                              \
+        return ppl::common::RC_UNSUPPORTED;                                                                        \
+    }                                                                                            \
+    ppl::common::Destructor __##___buffer_name___##_guard__([this, &___buffer_name___]() -> void { \
+        GetCudaDevice()->Free(&___buffer_name___);                                                \
+    });
+
 uint64_t ConvTransposeKernel::CalcTmpBufferSize(const KernelExecContext& ctx) const {
     auto x = ctx.GetInput<TensorImpl>(0);
     auto y = ctx.GetOutput<TensorImpl>(0);
@@ -49,6 +62,40 @@ ppl::common::RetCode ConvTransposeKernel::DoExecute(KernelExecContext* ctx) {
     TensorImpl* Y = ctx->GetOutput<TensorImpl>(0);
     const float* b_data = nullptr;
 
+    // convert filter only if the filter tensor is an output of another kernel
+    BufferDesc weight_buffer;
+    auto newshape = *W->GetShape();
+    if (!param_->extra_param.is_initializer_weight) {
+        auto align_size = 8;
+        int stride_h = param_->param.strides[0];
+        int stride_w = param_->param.strides[1];
+        int kernel_u = (newshape.GetDim(2) + stride_h - 1) / stride_h;
+        int kernel_v = (newshape.GetDim(3) + stride_w - 1) / stride_w;
+        int pattern_num = stride_h * stride_w;
+        newshape.SetDim(0, (newshape.GetDim(0) + align_size - 1) / align_size * align_size);
+        newshape.SetPadding1(1, (newshape.GetDim(1) + align_size - 1) / align_size * align_size - newshape.GetDim(1));
+        newshape.SetDim(2, pattern_num);
+        newshape.SetDim(3, kernel_u * kernel_v);
+        newshape.SetPadding1(0, (newshape.GetDim(0) + align_size - 1) / align_size * align_size - newshape.GetDim(0));
+
+        auto status = GetCudaDevice()->Realloc(newshape, &weight_buffer);
+        if (status != ppl::common::RC_SUCCESS) {
+            LOG(ERROR) << "alloc buffer for constant failed: " << GetRetCodeStr(status);
+            return status;
+        }
+        auto stream = GetStream();
+        auto size = PPLConvTransposeGetFilterBufSizeCudaFp16(W->GetShape());
+        ALLOC_BUFFERF_FOR_ALGO_SELECT(filter_temp_buffer, size, ALGO_MAX_TIME)
+        ALLOC_BUFFERF_FOR_ALGO_SELECT(filter_input_buffer, W->GetShape()->CalcBytesIncludingPadding(), ALGO_MAX_TIME)
+        auto filter_shape = *W->GetShape(); filter_shape.SetDataFormat(ppl::common::DATAFORMAT_NDARRAY);
+        GetCudaDevice()->GetDataConverter()->Convert(&filter_input_buffer, filter_shape, W->GetBufferDesc(), *W->GetShape());
+        PPLCUDAConvTransposeCvt(GetCudaDevice(), stream, filter_input_buffer.addr, filter_temp_buffer.addr,
+                                weight_buffer.addr, W->GetShape(), &param_->param);
+    }
+    ppl::common::Destructor __tmp_buffer_guard__([this, &weight_buffer]() -> void {
+        GetCudaDevice()->Free(&weight_buffer);
+    });
+
     if (ctx->GetInputCount() >= 3) {
         B = ctx->GetInput<TensorImpl>(2);
         b_data = B->GetBufferPtr<float>();
@@ -57,9 +104,9 @@ ppl::common::RetCode ConvTransposeKernel::DoExecute(KernelExecContext* ctx) {
     ConvertToForwardFuseParam(ctx, GetCudaDevice(), param_->extra_param.fuse_info, temp_fuse_param);
     CUDAModule* module = static_cast<CUDAModule*>(this->GetCommonParam()->module);
 
-    status = PPLCUDAConvTransposeForward(GetCudaDevice(), GetStream(), module, X->GetShape(),
-                                         X->GetBufferPtr(), W->GetBufferPtr(), b_data, Y->GetShape(),
-                                         Y->GetBufferPtr(), &param_->param, param_->extra_param.algo_info,
+    status = PPLCUDAConvTransposeForward(GetCudaDevice(), GetStream(), module, X->GetShape(), X->GetBufferPtr(),
+                                         param_->extra_param.is_initializer_weight ? (int4*)ctx->GetInput<TensorImpl>(1)->GetBufferPtr() : (int4*)weight_buffer.addr,
+                                         b_data, Y->GetShape(), Y->GetBufferPtr(), &param_->param, param_->extra_param.algo_info,
                                          temp_fuse_param, tmp_buffer);
 
     return status;
