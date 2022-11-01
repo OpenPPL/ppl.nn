600e899b988a0a67817dbe10e6c2af6acb6a8c0b
Moran <zmq232@126.com>
2022-11-01 07:40:52 +0000

    [cuda] gather kernel bugfix when axis < 0

--- src/ppl/nn/engines/cuda/kernels/onnx/gather_kernel.cc
+++ src/ppl/nn/engines/cuda/kernels/onnx/gather_kernel.cc
@@ -26,6 +26,8 @@ ppl::common::RetCode GatherKernel::DoExecute(KernelExecContext* ctx) {
     auto indices = ctx->GetInput<TensorImpl>(1);
     auto output = ctx->GetOutput<TensorImpl>(0);
 
+    int axis = param_->axis < 0 ? param_->axis + input->GetShape()->GetDimCount() : param_->axis;
+
     ppl::common::RetCode status = ppl::common::RC_SUCCESS;
     if (input->GetEdge()->CalcConsumerCount() == 1 && input->GetType() == TENSORTYPE_NORMAL &&
         input->GetShape()->GetDim(param_->axis) == 1 &&
@@ -34,7 +36,7 @@ ppl::common::RetCode GatherKernel::DoExecute(KernelExecContext* ctx) {
     } else {
         status =
             PPLCUDAGatherForwardImp(GetStream(), input->GetShape(), input->GetBufferPtr(), indices->GetShape(),
-                                    indices->GetBufferPtr(), output->GetShape(), output->GetBufferPtr(), param_->axis);
+                                    indices->GetBufferPtr(), output->GetShape(), output->GetBufferPtr(), axis);
     }
 
     return status;
