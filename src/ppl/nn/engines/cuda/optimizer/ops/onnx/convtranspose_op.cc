ab23957cd5fc4fedc79db0f27d40178877c76552
wangjianfei <wangjianfei@sensetime.com>
2022-11-09 05:08:03 -0500

    [cuda][fix]: possible empty ptx_code under jit mode

--- src/ppl/nn/engines/cuda/optimizer/ops/onnx/convtranspose_op.cc
+++ src/ppl/nn/engines/cuda/optimizer/ops/onnx/convtranspose_op.cc
@@ -107,7 +107,10 @@ KernelImpl* ConvTransposeOp::CreateKernelImpl() const {
 RetCode ConvTransposeOp::SerializeData(const pmx::SerializationContext& ctx, utils::DataStream* ds) const {
 #ifdef PPLNN_ENABLE_CUDA_JIT
     CUDAModule* module = static_cast<CUDAModule*>(GetCommparamModule());
-    auto ptx_code = module->GetSourceCode().second;
+    std::string ptx_code;
+    if (module) { // for specific algo, may no jit support
+        ptx_code = module->GetSourceCode().second;
+    }
 #else
     std::string ptx_code;
 #endif
@@ -144,12 +147,14 @@ RetCode ConvTransposeOp::DeserializeData(const pmx::DeserializationContext&, con
     }
 
 #ifdef PPLNN_ENABLE_CUDA_JIT
-    CUDAModule* cuda_module = new CUDAModule(); // delete later
-    cuda_module->SetSourceCode(param_.extra_param.algo_info.algo_name, ptx_code);
-    auto cuda_common_param = GetCommparam();
-    if (cuda_common_param->module) delete (CUDAModule*)cuda_common_param->module;
-    cuda_common_param->module = (void*)cuda_module;
-    pmx_module_created_ = true;
+    if (!ptx_code.empty()) { // for depthwise conv, no jit support
+        CUDAModule* cuda_module = new CUDAModule(); // delete later
+        cuda_module->SetSourceCode(param_.extra_param.algo_info.algo_name, ptx_code);
+        auto cuda_common_param = GetCommparam();
+        if (cuda_common_param->module) delete (CUDAModule*)cuda_common_param->module;
+        cuda_common_param->module = (void*)cuda_module;
+        pmx_module_created_ = true;
+    }
 #endif
     return RC_SUCCESS;
 }
--- src/ppl/nn/engines/cuda/optimizer/ops/onnx/matmul_op.cc
+++ src/ppl/nn/engines/cuda/optimizer/ops/onnx/matmul_op.cc
@@ -105,7 +105,10 @@ KernelImpl* MatMulOp::CreateKernelImpl() const {
 RetCode MatMulOp::SerializeData(const pmx::SerializationContext& ctx, utils::DataStream* ds) const {
 #ifdef PPLNN_ENABLE_CUDA_JIT
     CUDAModule* module = static_cast<CUDAModule*>(GetCommparamModule());
-    auto ptx_code = module->GetSourceCode().second;
+    std::string ptx_code;
+    if (module) { // for specific algo, may no jit support
+        ptx_code = module->GetSourceCode().second;
+    }
 #else
     std::string ptx_code;
 #endif
@@ -142,12 +145,14 @@ RetCode MatMulOp::DeserializeData(const pmx::DeserializationContext&, const void
     }
 
 #ifdef PPLNN_ENABLE_CUDA_JIT
-    CUDAModule* cuda_module = new CUDAModule(); // delete later
-    cuda_module->SetSourceCode(param_.extra_param.algo_info.algo_name, ptx_code);
-    auto cuda_common_param = GetCommparam();
-    if (cuda_common_param->module) delete (CUDAModule*)cuda_common_param->module;
-    cuda_common_param->module = (void*)cuda_module;
-    pmx_module_created_ = true;
+    if (!ptx_code.empty()) { // for depthwise conv, no jit support
+        CUDAModule* cuda_module = new CUDAModule(); // delete later
+        cuda_module->SetSourceCode(param_.extra_param.algo_info.algo_name, ptx_code);
+        auto cuda_common_param = GetCommparam();
+        if (cuda_common_param->module) delete (CUDAModule*)cuda_common_param->module;
+        cuda_common_param->module = (void*)cuda_module;
+        pmx_module_created_ = true;
+    }
 #endif
     return RC_SUCCESS;
 }
