add607d78cc675102de6e4899705a65201ad87b3
wangjianfei <wangjianfei@sensetime.com>
2022-11-17 20:05:58 +0800

    [cuda][opt]: vectorize clip impls

--- src/ppl/nn/engines/cuda/impls/src/unary/clip.cu
+++ src/ppl/nn/engines/cuda/impls/src/unary/clip.cu
@@ -30,11 +30,11 @@ __device__ __inline__ T ppl_scalar_clip(const T& in_val, float _min, float _max)
 
 #endif
 
-template <typename T>
+template <typename srcT, typename CalT, int Iter>
 __global__ void ppl_cukernel_clip_ndarray(
     const uint64_t num_elems,
-    const T* input,
-    T* output,
+    const srcT* input,
+    srcT* output,
     float _min,
     float _max)
 {
@@ -42,38 +42,53 @@ __global__ void ppl_cukernel_clip_ndarray(
     int64_t index = blockIdx.x * blockDim.x + threadIdx.x;
     if (index >= num_elems)
         return;
-    output[index] = ppl_scalar_clip<T>(input[index], _min, _max);
+    srcT in_val = input[index];
+    srcT out_val;
+    CalT* out_val_ptr = reinterpret_cast<CalT*>(&out_val);
+    CalT* in_val_ptr = reinterpret_cast<CalT*>(&in_val);
+    for (int it = 0; it < Iter; it++) {
+        out_val_ptr[it] = ppl_scalar_clip<CalT>(in_val_ptr[it], _min, _max);
+    }
+    output[index] = out_val;
 #endif
 }
 
-template <typename T>
+template <typename srcT, typename CalT, int Iter>
 __global__ void ppl_cukernel_clip_nhwc(
     const uint64_t num_elems,
     int channels,
     int pad_channels,
-    int chw,
+    int hwc,
     int hw,
-    const T* input,
-    T* output,
+    const srcT* input,
+    srcT* output,
     float _min,
     float _max)
 {
 #if __CUDA_ARCH__ >= 600 && __CUDACC_VER_MAJOR__ >= 9
-    int chw_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (chw_idx >= chw)
+    int hwc_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (hwc_idx >= hwc)
         return;
-    int c_idx     = chw_idx % channels;
-    int hw_idx    = chw_idx / channels;
+    int c_idx     = hwc_idx % channels;
+    int hw_idx    = hwc_idx / channels;
     int b_idx     = blockIdx.z;
     int64_t index = (b_idx * hw + hw_idx) * pad_channels + c_idx;
 
-    output[index] = ppl_scalar_clip<T>(input[index], _min, _max);
+    srcT in_val = input[index];
+    srcT out_val;
+    CalT* out_val_ptr = reinterpret_cast<CalT*>(&out_val);
+    CalT* in_val_ptr = reinterpret_cast<CalT*>(&in_val);
+    for (int it = 0; it < Iter; it++) {
+        out_val_ptr[it] = ppl_scalar_clip<CalT>(in_val_ptr[it], _min, _max);
+    }
+    output[index] = out_val;
 #endif
 }
+template <typename srcT, int Iter>
 __global__ void ppl_cukernel_clip_ndarray_int8(
     const uint64_t num_elems,
-    const char* input,
-    char* output,
+    const srcT* input,
+    srcT* output,
     float _min,
     float _max,
     float in_scale,
@@ -83,20 +98,28 @@ __global__ void ppl_cukernel_clip_ndarray_int8(
     if (index >= num_elems)
         return;
 
-    float tmp = (float)input[index] * in_scale;
-    float out_val = tmp < _min ? _min : tmp > _max ? _max : tmp;
-    int int_val = __float2int_rn(out_val * out_scale);
-    output[index] =  int_val < -128 ? -128 : int_val > 127 ? 127 : (char)int_val;
+    srcT in_val = input[index];
+    srcT out_val;
+    char* out_val_ptr = reinterpret_cast<char*>(&out_val);
+    char* in_val_ptr = reinterpret_cast<char*>(&in_val);
+    for (int it = 0; it < Iter; it++) {
+        float tmp = (float)in_val_ptr[it] * in_scale;
+        float out_val = tmp < _min ? _min : tmp > _max ? _max : tmp;
+        int int_val = __float2int_rn(out_val * out_scale);
+        out_val_ptr[it] = int_val < -128 ? -128 : int_val > 127 ? 127 : (char)int_val;
+    }
+    output[index] = out_val;
 }
 
+template <typename srcT, int Iter>
 __global__ void ppl_cudakernel_clip_nhwc_int8(
     const uint64_t num_elems,
     int channels,
     int pad_channels,
     int chw,
     int hw,
-    const char* input,
-    char* output,
+    const srcT* input,
+    srcT* output,
     float _min,
     float _max,
     float in_scale,
@@ -109,43 +132,18 @@ __global__ void ppl_cudakernel_clip_nhwc_int8(
     int b_idx = blockIdx.z;
     int64_t index = (b_idx * hw + hw_idx) * pad_channels + c_idx;
 
-    float tmp = (float)input[index] * in_scale;
-    float out_val = tmp < _min ? _min : tmp > _max ? _max : tmp;
-    int int_val = __float2int_rn(out_val * out_scale);
-    output[index] =  int_val < -128 ? -128 : int_val > 127 ? 127 : (char)int_val;
-}
-
-template <>
-__global__ void ppl_cukernel_clip_nhwc<float4>(
-    const uint64_t num_elems,
-    int channels,
-    int pad_channels,
-    int chw,
-    int hw,
-    const float4* input,
-    float4* output,
-    float _min,
-    float _max)
-{
-#if __CUDA_ARCH__ >= 600 && __CUDACC_VER_MAJOR__ >= 9
-    __shared__ float4 s_indata[256];
-    __shared__ float4 s_outdata[256];
-    int tid     = threadIdx.x;
-    int chw_idx = blockIdx.x * blockDim.x + tid;
-    if (chw_idx >= chw)
-        return;
-    int b_idx     = blockIdx.z;
-    int64_t index = b_idx * hw * pad_channels + chw_idx;
-    s_indata[tid] = input[index];
-
-    half* s_indata_half  = reinterpret_cast<half*>(s_indata);
-    half* s_outdata_half = reinterpret_cast<half*>(s_outdata);
-    for (int it = 0; it < 8; it++) {
-        int inner_idx             = tid * 8 + it;
-        s_outdata_half[inner_idx] = ppl_scalar_clip<half>(s_indata_half[inner_idx], _min, _max);
+    srcT in_val = input[index];
+    srcT out_val;
+    char* out_val_ptr = reinterpret_cast<char*>(&out_val);
+    char* in_val_ptr = reinterpret_cast<char*>(&in_val);
+    for (int it = 0; it < Iter; it++) {
+        float tmp = (float)in_val_ptr[it] * in_scale;
+        float out_val = tmp < _min ? _min : tmp > _max ? _max : tmp;
+        int int_val = __float2int_rn(out_val * out_scale);
+        out_val_ptr[it] = int_val < -128 ? -128 : int_val > 127 ? 127 : (char)int_val;
     }
-    output[index] = s_outdata[tid];
-#endif
+    output[index] = out_val;
+   
 }
 
 ppl::common::RetCode PPLCUDAClipForwardImp(
@@ -165,55 +163,103 @@ ppl::common::RetCode PPLCUDAClipForwardImp(
     int pad_channels   = output_shape->GetDim(1) + output_shape->GetPadding1(1);
     int height         = output_shape->GetDimCount() > 2 ? output_shape->GetDim(2) : 1;
     int width          = output_shape->GetDimCount() > 3 ? output_shape->GetDim(3) : 1;
+    int block_size = 256;
+    bool can_packed = (_min <= 0.f) && (0.f <= _max); // ensure f(0) = 0
 
-    if (output_shape->GetDataFormat() == ppl::common::DATAFORMAT_NDARRAY) {
-        int block_size = 256;
-        int grid_size  = (num_elems + block_size - 1) / block_size;
+    #define PACKED_EXEC(shift, srcT, CalT, Iter) \
+        int grid_size  = ((num_elems >> shift) + block_size - 1) / block_size;                  \
+        ppl_cukernel_clip_ndarray<srcT, CalT, Iter><<<grid_size, block_size, 0, stream>>>(      \
+            num_elems >> shift, (const srcT*)input, (srcT*)output, _min, _max);
+
+    #define PACKED_EXEC_INT8(shift, srcT, Iter) \
+        int grid_size  = ((num_elems >> shift) + block_size - 1) / block_size;                  \
+        ppl_cukernel_clip_ndarray_int8<srcT, Iter><<<grid_size, block_size, 0, stream>>>(      \
+            num_elems >> shift, (const srcT*)input, (srcT*)output, _min, _max, in_scale, out_scale);
+
+
+    if (can_packed || output_shape->GetDataFormat() == ppl::common::DATAFORMAT_NDARRAY) {
         if (output_shape->GetDataType() == ppl::common::DATATYPE_FLOAT32) {
-            ppl_cukernel_clip_ndarray<float><<<grid_size, block_size, 0, stream>>>(num_elems,
-                                                                                   (const float*)input,
-                                                                                   (float*)output,
-                                                                                   _min,
-                                                                                   _max);
+            if (num_elems & 0x1) {
+                PACKED_EXEC(0, float, float, 1)
+            } else if (num_elems & 0x2) {
+                PACKED_EXEC(1, float2, float, 2)
+            } else {
+                PACKED_EXEC(2, float4, float, 4)
+            }
         } else if (output_shape->GetDataType() == ppl::common::DATATYPE_FLOAT16) {
-            ppl_cukernel_clip_ndarray<half><<<grid_size, block_size, 0, stream>>>(num_elems,
-                                                                                  (const half*)input,
-                                                                                  (half*)output,
-                                                                                  _min,
-                                                                                  _max);
-
+            if (num_elems & 0x1) {
+                PACKED_EXEC(0, half, half, 1)
+            } else if (num_elems & 0x2) {
+                PACKED_EXEC(1, float, half, 2)
+            } else if (num_elems & 0x4) {
+                PACKED_EXEC(2, float2, half, 4)
+            } else {
+                PACKED_EXEC(3, float4, half, 8)
+            }
         } else {
-            ppl_cukernel_clip_ndarray_int8<<<grid_size, block_size, 0, stream>>>(num_elems,
-                                                                                  (const char*)input,
-                                                                                  (char*)output,
-                                                                                  _min,
-                                                                                  _max,
-                                                                                  in_scale,
-                                                                                  out_scale);
+            if (num_elems & 0x1) {
+                PACKED_EXEC_INT8(0, char, 1)
+            } else if (num_elems & 0x2) {
+                PACKED_EXEC_INT8(1, half, 2)
+            } else if (num_elems & 0x4) {
+                PACKED_EXEC_INT8(2, float, 4)
+            } else if (num_elems & 0x8) {
+                PACKED_EXEC_INT8(3, float2, 8)
+            } else {
+                PACKED_EXEC_INT8(4, float4, 16)
+            }
+
         }
     } else if (output_shape->GetDataFormat() == ppl::common::DATAFORMAT_NHWC8 ||
                output_shape->GetDataFormat() == ppl::common::DATAFORMAT_NHWC16) {
         int block_size = 256;
         dim3 grid_size;
-        int chw     = channels * height * width;
-        grid_size.x = (chw + block_size - 1) / block_size;
-        grid_size.z = batch;
+        grid_size.z = batch; grid_size.y = 1;
+
+    #define NHWC_EXEC(shift, srcT, CalT, Iter) \
+        int chw = (channels >> shift) * height * width;                                      \
+        grid_size.x = (chw + block_size - 1) / block_size;                  \
+        ppl_cukernel_clip_nhwc<srcT, CalT, Iter><<<grid_size, block_size, 0, stream>>>(      \
+            num_elems >> shift, channels >> shift, pad_channels >> shift, chw, height * width,   \
+            (const srcT*)input, (srcT*)output, _min, _max);
+
+    #define NHWC_EXEC_INT8(shift, srcT, Iter) \
+        int chw = (channels >> shift) * height * width;                                      \
+        grid_size.x = (chw + block_size - 1) / block_size;                  \
+        ppl_cudakernel_clip_nhwc_int8<srcT, Iter><<<grid_size, block_size, 0, stream>>>(      \
+            num_elems >> shift, channels >> shift, pad_channels >> shift, chw, height * width,   \
+            (const srcT*)input, (srcT*)output, _min, _max, in_scale, out_scale);
+
         if (output_shape->GetDataType() == ppl::common::DATATYPE_FLOAT32) {
-            ppl_cukernel_clip_nhwc<float><<<grid_size, block_size, 0, stream>>>(
-                num_elems, channels, pad_channels, channels * height * width, height * width, (const float*)input, (float*)output, _min, _max);
-        } else if (output_shape->GetDataType() == ppl::common::DATATYPE_FLOAT16) {
-            if (channels & 0x7) {
-                ppl_cukernel_clip_nhwc<half><<<grid_size, block_size, 0, stream>>>(
-                    num_elems, channels, pad_channels, channels * height * width, height * width, (const half*)input, (half*)output, _min, _max);
+            if (channels & 0x1) {
+                NHWC_EXEC(0, float, float, 1)
+            } else if (channels & 0x2) {
+                NHWC_EXEC(1, float2, float, 2)
             } else {
-                int pad_chw = pad_channels * height * width;
-                grid_size.x = (pad_chw + block_size - 1) / block_size;
-                ppl_cukernel_clip_nhwc<float4><<<grid_size, block_size, 0, stream>>>(
-                    num_elems >> 3, channels, pad_channels >> 3, pad_chw >> 3, height * width, (const float4*)input, (float4*)output, _min, _max);
+                NHWC_EXEC(2, float4, float, 4)
+            }
+        } else if (output_shape->GetDataType() == ppl::common::DATATYPE_FLOAT16) {
+            if (channels & 0x1) {
+                NHWC_EXEC(0, half, half, 1)
+            } else if (channels & 0x2) {
+                NHWC_EXEC(1, float, half, 2)
+            } else if (channels & 0x4) {
+                NHWC_EXEC(2, float2, half, 4)
+            } else {
+                NHWC_EXEC(3, float4, half, 8)
             }
         } else { // int8
-            ppl_cudakernel_clip_nhwc_int8<<<grid_size, block_size, 0, stream>>>(
-                num_elems, channels, pad_channels, channels * height * width, height * width, (const char*)input, (char*)output, _min, _max, in_scale, out_scale);
+            if (channels & 0x1) {
+                NHWC_EXEC_INT8(0, char, 1)
+            } else if (channels & 0x2) {
+                NHWC_EXEC_INT8(1, half, 2)
+            } else if (channels & 0x4) {
+                NHWC_EXEC_INT8(2, float, 4)
+            } else if (channels & 0x8) {
+                NHWC_EXEC_INT8(3, float2, 8)
+            } else {
+                NHWC_EXEC_INT8(4, float4, 16)
+            }
         }
     } else {
         return ppl::common::RC_UNSUPPORTED;
