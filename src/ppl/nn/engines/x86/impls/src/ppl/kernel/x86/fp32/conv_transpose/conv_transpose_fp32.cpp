3170fd2990092cf876d2616fb50c01f9694be625
alcanderian <alcanderian@gmail.com>
2022-12-13 20:56:31 +0800

    [x86][feature] support conv_transpose_1d

--- src/ppl/nn/engines/x86/impls/src/ppl/kernel/x86/fp32/conv_transpose/conv_transpose_fp32.cpp
+++ src/ppl/nn/engines/x86/impls/src/ppl/kernel/x86/fp32/conv_transpose/conv_transpose_fp32.cpp
@@ -21,11 +21,10 @@
 
 namespace ppl { namespace kernel { namespace x86 {
 
-uint64_t conv_transpose_ndarray_fp32_get_buffer_bytes(
+uint64_t conv_transpose_2d_ndarray_fp32_get_buffer_bytes(
     const ppl::common::isa_t isa,
+    const ppl::nn::TensorShape *src_shape,
     const int64_t group,
-    const int64_t src_h,
-    const int64_t src_w,
     const int64_t num_output,
     const int64_t kernel_h,
     const int64_t kernel_w,
@@ -35,6 +34,8 @@ uint64_t conv_transpose_ndarray_fp32_get_buffer_bytes(
     const int64_t pad_w)
 {
     (void) isa;
+    const int64_t src_h = src_shape->GetDim(2);
+    const int64_t src_w = src_shape->GetDim(3);
     const int64_t oc_per_grp = num_output / group;
     const bool do_col2im     = !(kernel_h == 1 && kernel_w == 1 && pad_h == 0 &&
                                 pad_w == 0 && stride_h == 1 && stride_w == 1);
@@ -42,16 +43,13 @@ uint64_t conv_transpose_ndarray_fp32_get_buffer_bytes(
     return col2im_len * sizeof(float);
 }
 
-ppl::common::RetCode conv_transpose_ndarray_fp32(
+ppl::common::RetCode conv_transpose_2d_ndarray_fp32(
     const ppl::common::isa_t isa,
-    const float *input,
+    const ppl::nn::TensorShape *src_shape,
+    const ppl::nn::TensorShape *dst_shape,
+    const float *src,
     const float *filter,
     const float *bias,
-    const int64_t src_h,
-    const int64_t src_w,
-    const int64_t dst_h,
-    const int64_t dst_w,
-    const int64_t batch,
     const int64_t group,
     const int64_t channels,
     const int64_t num_output,
@@ -63,11 +61,19 @@ ppl::common::RetCode conv_transpose_ndarray_fp32(
     const int64_t pad_w,
     const int64_t hole_h,
     const int64_t hole_w,
-    void *tmp_buffer,
-    float *output)
+    void *temp_buffer,
+    float *dst)
 {
+    const int64_t batch      = src_shape->GetDim(0);
+    const int64_t src_c      = src_shape->GetDim(1);
+    const int64_t src_h      = src_shape->GetDim(2);
+    const int64_t src_w      = src_shape->GetDim(3);
+    const int64_t dst_c      = dst_shape->GetDim(1);
+    const int64_t dst_h      = dst_shape->GetDim(2);
+    const int64_t dst_w      = dst_shape->GetDim(3);
     const int64_t ic_per_grp = channels / group;
     const int64_t oc_per_grp = num_output / group;
+
     const int64_t M = oc_per_grp * kernel_h * kernel_w;
     const int64_t N = src_h * src_w;
     const int64_t K = ic_per_grp;
@@ -76,7 +82,7 @@ ppl::common::RetCode conv_transpose_ndarray_fp32(
     const int64_t ldb   = N;
     const int64_t ldout = N;
 
-    float *col2im_buffer = (float*)tmp_buffer;
+    float *col2im_buffer = (float*)temp_buffer;
     const bool do_col2im = !(kernel_h == 1 && kernel_w == 1 && pad_h == 0 &&
                             pad_w == 0 && stride_h == 1 && stride_w == 1);
     const bool gemm_with_bias = !do_col2im && bias;
@@ -87,8 +93,110 @@ ppl::common::RetCode conv_transpose_ndarray_fp32(
         const float *l_flt = filter + g * oc_per_grp * ic_per_grp * kernel_h * kernel_w;
         const float *l_bias = bias ? bias + g * oc_per_grp : nullptr;
         for (int64_t b = 0; b < batch; ++b) {  
-            const float *l_src = input + (b * channels + g * ic_per_grp) * src_h * src_w;
-            float *l_dst       = output + (b * num_output + g * oc_per_grp) * dst_h * dst_w;
+            const float *l_src = src + (b * src_c + g * ic_per_grp) * src_h * src_w;
+            float *l_dst       = dst + (b * dst_c + g * oc_per_grp) * dst_h * dst_w;
+            float *l_col       = do_col2im ? col2im_buffer : l_dst;
+
+            auto ret = gemm_fp32(
+                isa, l_flt, l_src, l_bias, nullptr,
+                gemm_m_type::TRANS, gemm_m_type::NOTRANS,
+                gemm_with_bias ? gemm_v_type::COL_VEC : gemm_v_type::EMPTY,
+                gemm_m_type::EMPTY,
+                M, N, K, lda, ldb, ldout, 0,
+                1.0, 0.0, 1.0, 0.0, gemm_post::NONE, l_col);
+            if (ppl::common::RC_SUCCESS != ret) {
+                return ret;
+            }
+
+            if (do_col2im) {
+                col2im_func(
+                    l_col, l_bias, src_h, src_w, oc_per_grp,
+                    dst_h, dst_w, kernel_h, kernel_w,
+                    pad_h, pad_w, stride_h, stride_w,
+                    hole_h, hole_w, l_dst);
+            }
+        }
+    }
+
+    return ppl::common::RC_SUCCESS;
+}
+
+uint64_t conv_transpose_1d_ndarray_fp32_get_buffer_bytes(
+    const ppl::common::isa_t isa,
+    const ppl::nn::TensorShape *src_shape,
+    const int64_t group,
+    const int64_t num_output,
+    const int64_t kernel_w,
+    const int64_t stride_w,
+    const int64_t pad_w)
+{
+    (void) isa;
+    const int64_t kernel_h = 1;
+    const int64_t stride_h = 1;
+    const int64_t pad_h    = 0;
+    const int64_t src_h    = 1;
+
+    const int64_t src_w = src_shape->GetDim(2);
+    const int64_t oc_per_grp = num_output / group;
+    const bool do_col2im     = !(kernel_h == 1 && kernel_w == 1 && pad_h == 0 &&
+                                pad_w == 0 && stride_h == 1 && stride_w == 1);
+    const uint64_t col2im_len = !do_col2im ? 0 : uint64_t(oc_per_grp) * kernel_h * kernel_w * src_h * src_w;
+    return col2im_len * sizeof(float);
+}
+
+ppl::common::RetCode conv_transpose_1d_ndarray_fp32(
+    const ppl::common::isa_t isa,
+    const ppl::nn::TensorShape *src_shape,
+    const ppl::nn::TensorShape *dst_shape,
+    const float *src,
+    const float *filter,
+    const float *bias,
+    const int64_t group,
+    const int64_t channels,
+    const int64_t num_output,
+    const int64_t kernel_w,
+    const int64_t stride_w,
+    const int64_t pad_w,
+    const int64_t hole_w,
+    void *temp_buffer,
+    float *dst)
+{
+    const int64_t kernel_h = 1;
+    const int64_t stride_h = 1;
+    const int64_t pad_h    = 0;
+    const int64_t hole_h   = 1;
+    const int64_t src_h    = 1;
+    const int64_t dst_h    = 1;
+
+    const int64_t batch      = src_shape->GetDim(0);
+    const int64_t src_c      = src_shape->GetDim(1);
+    const int64_t src_w      = src_shape->GetDim(2);
+    const int64_t dst_c      = dst_shape->GetDim(1);
+    const int64_t dst_w      = dst_shape->GetDim(2);
+    const int64_t ic_per_grp = channels / group;
+    const int64_t oc_per_grp = num_output / group;
+
+    const int64_t M = oc_per_grp * kernel_h * kernel_w;
+    const int64_t N = src_h * src_w;
+    const int64_t K = ic_per_grp;
+
+    const int64_t lda   = M;
+    const int64_t ldb   = N;
+    const int64_t ldout = N;
+
+    float *col2im_buffer = (float*)temp_buffer;
+    const bool do_col2im = !(kernel_h == 1 && kernel_w == 1 && pad_h == 0 &&
+                            pad_w == 0 && stride_h == 1 && stride_w == 1);
+    const bool gemm_with_bias = !do_col2im && bias;
+
+    auto col2im_func = (isa & ppl::common::ISA_X86_AVX) ? col2im2d_ndarray_fp32_avx : col2im2d_ndarray_fp32_sse;
+
+    for (int64_t g = 0; g < group; ++g) {
+        const float *l_flt = filter + g * oc_per_grp * ic_per_grp * kernel_h * kernel_w;
+        const float *l_bias = bias ? bias + g * oc_per_grp : nullptr;
+        for (int64_t b = 0; b < batch; ++b) {  
+            const float *l_src = src + (b * src_c + g * ic_per_grp) * src_h * src_w;
+            float *l_dst       = dst + (b * dst_c + g * oc_per_grp) * dst_h * dst_w;
             float *l_col       = do_col2im ? col2im_buffer : l_dst;
 
             auto ret = gemm_fp32(
--- src/ppl/nn/engines/x86/kernels/onnx/convtranspose_kernel.cc
+++ src/ppl/nn/engines/x86/kernels/onnx/convtranspose_kernel.cc
@@ -25,15 +25,24 @@ uint64_t ConvTransposeKernel::CalcTmpBufferSize(const KernelExecContext& ctx) co
     auto X = ctx.GetInput<TensorImpl>(0);
     auto W = ctx.GetInput<TensorImpl>(1);
 
-    const int64_t src_h = X->GetShape()->GetDim(2);
-    const int64_t src_w = X->GetShape()->GetDim(3);
     const int64_t num_output = W->GetShape()->GetDim(1) * param_->group;
+    const uint32_t kernel_dims = W->GetShape()->GetDimCount() - 2;
 
-    return ppl::kernel::x86::conv_transpose_ndarray_fp32_get_buffer_bytes(
-        GetISA(), param_->group, src_h, src_w, num_output,
-        param_->kernel_shape[0], param_->kernel_shape[1],
-        param_->strides[0], param_->strides[1],
-        param_->pads[0], param_->pads[1]);
+    if (kernel_dims == 2) {
+        return ppl::kernel::x86::conv_transpose_2d_ndarray_fp32_get_buffer_bytes(
+            GetISA(), X->GetShape(), param_->group, num_output,
+            param_->kernel_shape[0], param_->kernel_shape[1],
+            param_->strides[0], param_->strides[1],
+            param_->pads[0], param_->pads[1]);
+    }
+    if (kernel_dims == 1) {
+        return ppl::kernel::x86::conv_transpose_1d_ndarray_fp32_get_buffer_bytes(
+            GetISA(), X->GetShape(), param_->group, num_output,
+            param_->kernel_shape[0],
+            param_->strides[0],
+            param_->pads[0]);
+    }
+    return 0;
 }
 
 ppl::common::RetCode ConvTransposeKernel::DoExecute(KernelExecContext* ctx) {
@@ -41,36 +50,52 @@ ppl::common::RetCode ConvTransposeKernel::DoExecute(KernelExecContext* ctx) {
     PPLNN_X86_REQUIRED_INPUT(W, 1);
     PPLNN_X86_OPTIONAL_INPUT(B, 2);
     PPLNN_X86_REQUIRED_OUTPUT(Y, 0);
-    const float* b_data = nullptr;
-
-    int64_t num_output = W->GetShape()->GetDim(1) * param_->group;
-    if (B) {
-        b_data = B->GetBufferPtr<float>();
-    }
 
     PPLNN_X86_DEBUG_TRACE("Op: %s\n", GetName().c_str());
     PPLNN_X86_DEBUG_TRACE("Input [X]:\n");
     PPL_X86_TENSOR_PRINT_DEBUG_MSG(X);
     PPLNN_X86_DEBUG_TRACE("Input [W]:\n");
     PPL_X86_TENSOR_PRINT_DEBUG_MSG(W);
+
+    const float* b_data = nullptr;
     if (B) {
+        b_data = B->GetBufferPtr<float>();
         PPLNN_X86_DEBUG_TRACE("Input [B]:\n");
         PPL_X86_TENSOR_PRINT_DEBUG_MSG(B);
     }
 
-    PPLNN_X86_DEBUG_TRACE("kernel_shape: %d %d\n", param_->kernel_shape[0], param_->kernel_shape[1]);
-    PPLNN_X86_DEBUG_TRACE("dilations: %d %d\n", param_->dilations[0], param_->dilations[1]);
-    PPLNN_X86_DEBUG_TRACE("strides: %d %d\n", param_->strides[0], param_->strides[1]);
-    PPLNN_X86_DEBUG_TRACE("pads: %d %d %d %d\n", param_->pads[0], param_->pads[1], param_->pads[2], param_->pads[3]);
-    PPLNN_X86_DEBUG_TRACE("group: %ld\n", param_->group);
+    int64_t num_output = W->GetShape()->GetDim(1) * param_->group;
+    const int64_t channels = X->GetShape()->GetDim(1);
+    const uint32_t kernel_dims = W->GetShape()->GetDimCount() - 2;
+
+    if (kernel_dims == 1) {
+        PPLNN_X86_DEBUG_TRACE("kernel_shape: %d\n", param_->kernel_shape[0]);
+        PPLNN_X86_DEBUG_TRACE("dilations: %d\n", param_->dilations[0]);
+        PPLNN_X86_DEBUG_TRACE("strides: %d\n", param_->strides[0]);
+        PPLNN_X86_DEBUG_TRACE("pads: %d %d\n", param_->pads[0], param_->pads[1]);
+    }
+    if (kernel_dims == 2) {
+        PPLNN_X86_DEBUG_TRACE("kernel_shape: %d %d\n", param_->kernel_shape[0], param_->kernel_shape[1]);
+        PPLNN_X86_DEBUG_TRACE("dilations: %d %d\n", param_->dilations[0], param_->dilations[1]);
+        PPLNN_X86_DEBUG_TRACE("strides: %d %d\n", param_->strides[0], param_->strides[1]);
+        PPLNN_X86_DEBUG_TRACE("pads: %d %d %d %d\n", param_->pads[0], param_->pads[1], param_->pads[2], param_->pads[3]);
+    }
+    PPLNN_X86_DEBUG_TRACE("group: %d\n", param_->group);
+    PPLNN_X86_DEBUG_TRACE("num_output: %d\n", num_output);
     PPLNN_X86_DEBUG_TRACE("isa: %u\n", GetISA());
 
-    const int64_t batch = X->GetShape()->GetDim(0);
-    const int64_t channels = X->GetShape()->GetDim(1);
-    const int64_t src_h = X->GetShape()->GetDim(2);
-    const int64_t src_w = X->GetShape()->GetDim(3);
-    const int64_t dst_h = Y->GetShape()->GetDim(2);
-    const int64_t dst_w = Y->GetShape()->GetDim(3);
+    if (X->GetShape()->GetDataType() != ppl::common::DATATYPE_FLOAT32 ||
+        X->GetShape()->GetDataFormat() != ppl::common::DATAFORMAT_NDARRAY) {
+        LOG(ERROR) << "only support fp32 ndarray now.";
+        return ppl::common::RC_UNSUPPORTED;
+    }
+
+    for (uint32_t i = 0; i < kernel_dims; ++i) {
+        if (param_->pads[i] != param_->pads[i + kernel_dims]) {
+            LOG(ERROR) << "only support symmetrical pads.";
+            return ppl::common::RC_UNSUPPORTED;
+        }
+    }
 
     PPLNN_X86_REALLOC_TENSOR_BUFFER(Y);
     PPLNN_X86_DEBUG_TRACE("Output [Y]:\n");
@@ -90,25 +115,25 @@ ppl::common::RetCode ConvTransposeKernel::DoExecute(KernelExecContext* ctx) {
     auto tmp_buffer = tmp_buffer_desc.addr;
     PPLNN_X86_DEBUG_TRACE("buffer: %p\n", tmp_buffer);
 
-    const auto data_format = X->GetShape()->GetDataFormat();
-    const auto data_type = X->GetShape()->GetDataType();
-
-    if (data_type == ppl::common::DATATYPE_FLOAT32) {
-        if (data_format == ppl::common::DATAFORMAT_NDARRAY) {
-            return kernel::x86::conv_transpose_ndarray_fp32(
-                GetISA(), X->GetBufferPtr<float>(), W->GetBufferPtr<float>(), b_data,
-                src_h, src_w, dst_h, dst_w,
-                batch, param_->group, channels, num_output,
-                param_->kernel_shape[0], param_->kernel_shape[1],
-                param_->strides[0], param_->strides[1],
-                param_->pads[0], param_->pads[1],
-                param_->dilations[0], param_->dilations[1],
-                tmp_buffer, Y->GetBufferPtr<float>());
-        } else {
-            LOG(ERROR) << "unsupported data format: " << ppl::common::GetDataFormatStr(data_format) << ".";
-        }
-    } else {
-        LOG(ERROR) << "unsupported data type: " << ppl::common::GetDataTypeStr(data_type) << ".";
+    if (kernel_dims == 1) {
+        return kernel::x86::conv_transpose_1d_ndarray_fp32(
+            GetISA(), X->GetShape(), Y->GetShape(),
+            X->GetBufferPtr<float>(), W->GetBufferPtr<float>(), b_data,
+            param_->group, channels, num_output,
+            param_->kernel_shape[0], param_->strides[0],
+            param_->pads[0], param_->dilations[0],
+            tmp_buffer, Y->GetBufferPtr<float>());
+    }
+    if (kernel_dims == 2) {
+        return kernel::x86::conv_transpose_2d_ndarray_fp32(
+            GetISA(), X->GetShape(), Y->GetShape(),
+            X->GetBufferPtr<float>(), W->GetBufferPtr<float>(), b_data,
+            param_->group, channels, num_output,
+            param_->kernel_shape[0], param_->kernel_shape[1],
+            param_->strides[0], param_->strides[1],
+            param_->pads[0], param_->pads[1],
+            param_->dilations[0], param_->dilations[1],
+            tmp_buffer, Y->GetBufferPtr<float>());
     }
     return ppl::common::RC_UNSUPPORTED;
 }
