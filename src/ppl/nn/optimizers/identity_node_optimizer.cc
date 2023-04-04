9d4f65f50ea2f5195c6b94acb4cd670642d0c382
alcanderian <alcanderian@gmail.com>
2023-04-04 19:36:06 +0800

    [opt] Optimize IdentityNodeOptimizer

--- src/ppl/nn/optimizers/identity_node_optimizer.cc
+++ src/ppl/nn/optimizers/identity_node_optimizer.cc
@@ -22,6 +22,28 @@ using namespace ppl::common;
 
 namespace ppl { namespace nn {
 
+inline bool IsGraphOutput(const ir::Graph* graph, edgeid_t edge_id) {
+    for (uint32_t i = 0; i < graph->topo->GetOutputCount(); i++) {
+        if (graph->topo->GetOutput(i) == edge_id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+inline bool IsGraphInput(const ir::Graph* graph, edgeid_t edge_id) {
+    for (uint32_t i = 0; i < graph->topo->GetInputCount(); i++) {
+        if (graph->topo->GetInput(i) == edge_id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+inline bool IsConstant(const ir::Graph* graph, edgeid_t edge_id) {
+    return graph->data->constants.find(edge_id) != graph->data->constants.end();
+}
+
 // move identity op's output edge to graph identity & delete identity op
 ppl::common::RetCode IdentityNodeOptimizer::Optimize(ir::Graph* graph) const {
     for (auto it = graph->topo->CreateNodeIter(); it->IsValid(); it->Forward()) {
@@ -34,21 +56,37 @@ ppl::common::RetCode IdentityNodeOptimizer::Optimize(ir::Graph* graph) const {
                 LOG(ERROR) << "cannot find identity node[" << identity_node->GetName() << "]'s input/output edge.";
                 return RC_NOT_FOUND;
             }
-            
-            if (out_edge->CalcConsumerCount() == 0) {
+
+            const bool in_edge_is_input = IsGraphInput(graph, in_edge->GetId());
+            const bool in_edge_is_constant = IsConstant(graph, in_edge->GetId());
+            const bool out_edge_is_output = IsGraphOutput(graph, out_edge->GetId());
+            if ((in_edge_is_input || in_edge_is_constant) && out_edge_is_output) {
                 continue;
             }
-            auto next_node_id = out_edge->CreateConsumerIter().Get();
-            auto next_node = graph->topo->GetNode(next_node_id);
-            next_node->ReplaceInput(out_edge->GetId(), in_edge->GetId());
 
-            // delete constant node
-            in_edge->DelConsumer(identity_node->GetId());
-            in_edge->AddConsumer(next_node_id);
-            out_edge->DelConsumer(next_node_id);
-            graph->topo->DelNode(identity_node->GetId());
-            if (out_edge->CalcConsumerCount() == 0) {
+            if (in_edge_is_input || in_edge_is_constant) {
+                in_edge->DelConsumer(identity_node->GetId());
+                graph->topo->DelNode(identity_node->GetId());
+
+                auto iter = out_edge->CreateConsumerIter();
+                while (iter.IsValid()) {
+                    auto next_node_id = iter.Get();
+                    auto next_node = graph->topo->GetNode(next_node_id);
+                    next_node->ReplaceInput(out_edge->GetId(), in_edge->GetId());
+                    in_edge->AddConsumer(next_node_id);
+                    iter.Forward();
+                }
+
                 graph->topo->DelEdge(out_edge->GetId());
+            } else {
+                auto prev_node_id = in_edge->GetProducer();
+                auto prev_node = graph->topo->GetNode(prev_node_id);
+
+                prev_node->ReplaceOutput(in_edge->GetId(), out_edge->GetId());
+                out_edge->SetProducer(prev_node_id);
+
+                graph->topo->DelEdge(in_edge->GetId());
+                graph->topo->DelNode(identity_node->GetId());
             }
         }
     }
