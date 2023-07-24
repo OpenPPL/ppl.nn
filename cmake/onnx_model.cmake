d94dc4a6e94c2c71306e293d0c8f095cbc50a98d
ouguoyu <ouguoyu@sensetime.com>
2023-07-24 10:36:55 +0800

    [misc] use lib dep for different pb generated files

--- cmake/onnx_model.cmake
+++ cmake/onnx_model.cmake
@@ -1,13 +1,12 @@
 file(GLOB_RECURSE __PPLNN_MODEL_ONNX_SRC__ src/ppl/nn/models/onnx/*.cc)
 
-if(PPLNN_PROTOBUF_VERSION AND NOT PPLNN_ONNX_GENERATED_DIR)
-   message(FATAL_ERROR "`PPLNN_PROTOBUF_VERSION` is set to be [${PPLNN_PROTOBUF_VERSION}], but `PPLNN_ONNX_GENERATED_DIR` is not set.")
+if(PPLNN_PROTOBUF_VERSION AND NOT PPLNN_ONNX_GENERATED_LIBS)
+   message(FATAL_ERROR "`PPLNN_PROTOBUF_VERSION` is set to be [${PPLNN_PROTOBUF_VERSION}], but `PPLNN_ONNX_GENERATED_LIBS` is not set.")
 endif()
 
 # replace default *.pb.* files
-if(PPLNN_ONNX_GENERATED_DIR)
+if(PPLNN_ONNX_GENERATED_LIBS)
     list(REMOVE_ITEM __PPLNN_MODEL_ONNX_SRC__ ${PROJECT_SOURCE_DIR}/src/ppl/nn/models/onnx/generated/onnx.pb.cc)
-    list(APPEND __PPLNN_MODEL_ONNX_SRC__ ${PPLNN_ONNX_GENERATED_DIR}/onnx.pb.cc)
 endif()
 
 # if external sources are set, remove `default_register_resources.cc`
@@ -16,12 +15,13 @@ if(PPLNN_SOURCE_EXTERNAL_ONNX_MODEL_SOURCES)
 endif()
 
 add_library(pplnn_onnx_static STATIC ${__PPLNN_MODEL_ONNX_SRC__} ${PPLNN_SOURCE_EXTERNAL_ONNX_MODEL_SOURCES})
+
 unset(__PPLNN_MODEL_ONNX_SRC__)
 
-if(PPLNN_ONNX_GENERATED_DIR)
-    target_include_directories(pplnn_onnx_static PUBLIC ${PPLNN_ONNX_GENERATED_DIR})
+if(PPLNN_ONNX_GENERATED_LIBS)
+    target_link_libraries(pplnn_onnx_static PRIVATE ${PPLNN_ONNX_GENERATED_LIBS})
 else()
-    target_include_directories(pplnn_onnx_static PUBLIC src/ppl/nn/models/onnx/generated)
+    target_include_directories(pplnn_onnx_static PRIVATE ${PROJECT_SOURCE_DIR}/src/ppl/nn/models/onnx/generated)
 endif()
 
 target_compile_definitions(pplnn_onnx_static PUBLIC PPLNN_ENABLE_ONNX_MODEL)
