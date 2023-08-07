484f21ee32f49d4dd553e131c45e3cd92121afcd
ouguoyu <ouguoyu@sensetime.com>
2023-08-07 21:34:12 +0800

    s

--- src/ppl/nn/engines/cuda/engine.cc
+++ src/ppl/nn/engines/cuda/engine.cc
@@ -14,7 +14,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 #include "ppl/nn/engines/cuda/engine.h"
 
 #include <stdarg.h>
