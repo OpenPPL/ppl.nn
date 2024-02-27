5309be1b2ef4ef8c10a58ccce0a34d9609606192
alcanderian <alcanderian@gmail.com>
2024-02-27 16:17:20 +0800

    [misc] check conv channels

--- src/ppl/nn/engines/x86/optimizer/ops/onnx/conv_op.cc
+++ src/ppl/nn/engines/x86/optimizer/ops/onnx/conv_op.cc
@@ -64,6 +64,13 @@ RetCode ConvOp::DoInit(const OptKernelOptions& options) {
     }
 
     infer_dims_func_ = [this](InputOutputInfo* info) -> RetCode {
+        auto x = info->GetInput<TensorImpl>(0)->GetShape();
+        auto w = info->GetInput<TensorImpl>(1)->GetShape();
+        if (x->GetDim(1) != w->GetDim(1)) {
+            LOG(ERROR) << "input tensor's channels(" << x->GetDim(1) <<
+                ") and weight's channels(" << w->GetDim(1) << ") not match.";
+            return ppl::common::RC_INVALID_VALUE;
+        }
         return onnx::ReshapeConv(info, param_.get());
     };
 
