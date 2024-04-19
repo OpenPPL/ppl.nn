11a202c98b042de1e732483e983ff68030ebf720
ouguoyu <ouguoyu@sensetime.com>
2024-04-19 15:19:19 +0800

    [misc] use protoc to generate *.pb.* if not cross compiling

--- cmake/llm_cuda.cmake
+++ cmake/llm_cuda.cmake
@@ -2,33 +2,6 @@ if(NOT TARGET libprotobuf)
     hpcc_populate_dep(protobuf)
 endif()
 
-if(NOT PPL_LLM_PROTOC_EXECUTABLE)
-    set(PPL_LLM_PROTOC_EXECUTABLE ${protobuf_BINARY_DIR}/protoc)
-endif()
-
-if(NOT PPLNN_ONNX_GENERATED_LIBS)
-    # generate new onnx.pb.* for pplnn
-    set(__LLM_GENERATED_DIR__ ${CMAKE_CURRENT_BINARY_DIR}/generated)
-    file(MAKE_DIRECTORY ${__LLM_GENERATED_DIR__})
-
-    set(__PROTO_DIR__ ${CMAKE_CURRENT_SOURCE_DIR}/src/ppl/nn/models/onnx/proto)
-    set(__ONNX_GENERATED_FILES__ "${__LLM_GENERATED_DIR__}/onnx.pb.h;${__LLM_GENERATED_DIR__}/onnx.pb.cc")
-    add_custom_command(
-        OUTPUT ${__ONNX_GENERATED_FILES__}
-        COMMAND ${PPL_LLM_PROTOC_EXECUTABLE}
-        ARGS --cpp_out ${__LLM_GENERATED_DIR__} -I ${__PROTO_DIR__}
-        ${__PROTO_DIR__}/onnx.proto
-        DEPENDS protoc ${__PROTO_DIR__}/onnx.proto)
-    add_library(pplnn_onnx_generated_static STATIC ${__ONNX_GENERATED_FILES__})
-    target_link_libraries(pplnn_onnx_generated_static PUBLIC libprotobuf)
-    target_include_directories(pplnn_onnx_generated_static PUBLIC ${__LLM_GENERATED_DIR__})
-    set(PPLNN_ONNX_GENERATED_LIBS pplnn_onnx_generated_static)
-
-    unset(__ONNX_GENERATED_FILES__)
-    unset(__PROTO_DIR__)
-    unset(__LLM_GENERATED_DIR__)
-endif()
-
 include(${HPCC_DEPS_DIR}/hpcc/cmake/cuda-common.cmake)
 
 hpcc_populate_dep(ppl.llm.kernel.cuda)
--- cmake/onnx_model.cmake
+++ cmake/onnx_model.cmake
@@ -1,12 +1,37 @@
 file(GLOB_RECURSE __PPLNN_MODEL_ONNX_SRC__ src/ppl/nn/models/onnx/*.cc)
 list(REMOVE_ITEM __PPLNN_MODEL_ONNX_SRC__ ${PROJECT_SOURCE_DIR}/src/ppl/nn/models/onnx/generated/onnx.pb.cc)
 
-if(PPLNN_PROTOBUF_VERSION AND NOT PPLNN_ONNX_GENERATED_LIBS)
-   message(FATAL_ERROR "`PPLNN_PROTOBUF_VERSION` is set to be [${PPLNN_PROTOBUF_VERSION}], but `PPLNN_ONNX_GENERATED_LIBS` is not set.")
-endif()
-
 if(NOT TARGET libprotobuf)
     include(cmake/protobuf.cmake)
+endif()
+
+# use protoc to generate *.pb.*
+if(NOT CMAKE_CROSSCOMPILING)
+    if(NOT PPLNN_PROTOC_EXECUTABLE)
+        set(PPLNN_PROTOC_EXECUTABLE ${protobuf_BINARY_DIR}/protoc)
+    endif()
+
+    set(__GENERATED_DIR__ ${CMAKE_CURRENT_BINARY_DIR}/generated)
+    file(MAKE_DIRECTORY ${__GENERATED_DIR__})
+
+    set(__PROTO_DIR__ ${CMAKE_CURRENT_SOURCE_DIR}/src/ppl/nn/models/onnx/proto)
+    set(__ONNX_GENERATED_FILES__ "${__GENERATED_DIR__}/onnx.pb.h;${__GENERATED_DIR__}/onnx.pb.cc")
+    add_custom_command(
+        OUTPUT ${__ONNX_GENERATED_FILES__}
+        COMMAND ${PPLNN_PROTOC_EXECUTABLE}
+        ARGS --cpp_out ${__GENERATED_DIR__} -I ${__PROTO_DIR__}
+        ${__PROTO_DIR__}/onnx.proto
+        DEPENDS protoc ${__PROTO_DIR__}/onnx.proto)
+    add_library(pplnn_onnx_generated_static STATIC ${__ONNX_GENERATED_FILES__})
+    target_link_libraries(pplnn_onnx_generated_static PUBLIC libprotobuf)
+    target_include_directories(pplnn_onnx_generated_static PUBLIC ${__GENERATED_DIR__})
+    set(PPLNN_ONNX_GENERATED_LIBS pplnn_onnx_generated_static)
+
+    unset(__ONNX_GENERATED_FILES__)
+    unset(__PROTO_DIR__)
+    unset(__GENERATED_DIR__)
+elseif(PPLNN_PROTOBUF_VERSION AND NOT PPLNN_ONNX_GENERATED_LIBS)
+   message(FATAL_ERROR "`PPLNN_PROTOBUF_VERSION` is set to be [${PPLNN_PROTOBUF_VERSION}], but `PPLNN_ONNX_GENERATED_LIBS` is not set.")
 endif()
 
 # use default *.pb.* files
