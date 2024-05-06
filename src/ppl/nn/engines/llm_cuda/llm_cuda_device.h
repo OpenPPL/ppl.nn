bc45e6111d5e03c836c16f597dd83a95a8680436
Liheng Jian <jianliheng@sensetime.com>
2024-05-06 19:57:39 +0800

    [refactor][llm_cuda]refactor vision_embedding op.

--- src/ppl/nn/engines/llm_cuda/llm_cuda_device.h
+++ src/ppl/nn/engines/llm_cuda/llm_cuda_device.h
@@ -124,10 +124,6 @@ public:
         return &cublas_algo_cache_;
     }
 
-    void SetCudnnHandle(cudnnHandle_t cudnn_handle) {
-        cudnn_handle_ = cudnn_handle;
-    };
-
     cudnnHandle_t GetCudnnHandle() const {
         return cudnn_handle_;
     }
