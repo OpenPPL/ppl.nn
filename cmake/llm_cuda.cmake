0f8ca953c1b1052914506053b368c930e3588e96
Liheng Jian <jianliheng@sensetime.com>
2024-05-17 09:15:21 +0800

    [opt]swith cudnn dependency to ppl.llm.kernel.cuda.

--- cmake/llm_cuda.cmake
+++ cmake/llm_cuda.cmake
@@ -1,10 +1,3 @@
-if(NOT CUDNN_INCLUDE_DIR)
-    message(FATAL_ERROR "`CUDNN_INCLUDE_DIR` is not set.")
-endif()
-if(NOT CUDNN_LIBRARY)
-    message(FATAL_ERROR "`CUDNN_LIBRARY` is not set.")
-endif()
-
 if(NOT TARGET libprotobuf)
     hpcc_populate_dep(protobuf)
 endif()
@@ -17,9 +10,6 @@ file(GLOB_RECURSE __SRC__ src/ppl/nn/engines/llm_cuda/*.cc)
 add_library(ppl_llm_cuda_static ${__SRC__})
 target_link_libraries(ppl_llm_cuda_static PUBLIC pplnn_basic_static pplkernelcuda_static)
 target_compile_definitions(ppl_llm_cuda_static PUBLIC PPLNN_USE_LLM_CUDA)
-
-target_link_libraries(ppl_llm_cuda_static PUBLIC ${CUDNN_LIBRARY})
-target_include_directories(ppl_llm_cuda_static PUBLIC ${CUDNN_INCLUDE_DIR})
 
 if(PPLNN_CUDA_ENABLE_NCCL)
     target_compile_definitions(ppl_llm_cuda_static PUBLIC PPLNN_CUDA_ENABLE_NCCL)
